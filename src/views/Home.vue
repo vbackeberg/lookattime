781d3799d21fb6ed9d41c26240a4cd0b960763ed
Valerian Backeberg <extern.backeberg_valerian@allianz.de>
2020-07-27 21:25:08 +0200

    remove debug logs in zoom functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,12 +61,12 @@ export default Vue.extend({
 
     zoomIn(e: WheelEvent) {
       this.zoomLevel += 0.1;
-      this.determineNewPosition(1.1, e.pageX);
+      this.determineNewPosition(1.07, e.pageX);
     },
 
     zoomOut(e: WheelEvent) {
       this.zoomLevel -= 0.1;
-      this.determineNewPosition(0.9, e.pageX);
+      this.determineNewPosition(0.92, e.pageX);
     },
 
     determineNewPosition(zoomFactor: number, mousePosition: number) {
@@ -93,8 +93,6 @@ export default Vue.extend({
     },
 
     normalizePositions() {
-      console.log("lowest box position center: " + this.lowestBox.positionLeft);
-
       if (this.lowestBox.positionLeft < 0) {
         this.extendSpace();
       } else if (
@@ -102,47 +100,26 @@ export default Vue.extend({
         window.pageXOffset > 0
       ) {
         this.cutSpace();
-      } else {
-        console.log("not normalizing.");
-      }
-      console.log("----------------------------------------------------");
+      } 
     },
 
     cutSpace() {
-      console.log("normalizing...");
-      console.log("shrinking...");
-      console.log("pageXOffset " + window.pageXOffset);
-
       const distance = window.pageXOffset * -1;
-      console.log("distance " + distance);
-
       window.scrollBy(distance, 0);
-
+      
       this.boxes.forEach((box) => {
-        console.log("box " + box.id + " old pos " + box.positionLeft);
         box.positionLeft += distance;
-        console.log("box " + box.id + " new pos " + box.positionLeft);
       });
     },
 
     extendSpace() {
-      console.log("normalizing...");
-      console.log("extending...");
-
       const lowestBoxPositionLeft = this.lowestBox.positionLeft;
-      console.log("lowestBoxPositionLeft " + lowestBoxPositionLeft);
-
+      
       this.boxes.forEach((box) => {
-        console.log("box " + box.id + " old pos " + box.positionLeft);
         box.positionLeft -= lowestBoxPositionLeft;
-        console.log("box " + box.id + " new pos " + box.positionLeft);
       });
-
-      console.log("screenX " + window.screenX);
-
+      
       const screenXNew = window.screenX - lowestBoxPositionLeft;
-      console.log("screenXNew " + screenXNew);
-
       window.scrollBy(screenXNew, 0);
     },
 

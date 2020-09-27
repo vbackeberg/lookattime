0fdd24ad6d0667287c0534908888e7b176d9749b
Valerian Backeberg <valerian@backebergs.de>
2020-09-27 20:31:48 +0200

    Implement new singleton repositioner. It retrieves boxes and spacers from store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,5 @@
+import BoxModel from "@/models/box-model";
+import SpacerModel from "@/models/spacer-model";
 import Vue from "vue";
 import Vuex from "vuex";
 
@@ -5,12 +7,25 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    timelineZero: 500 //TODO initiate at center of screen
+    timelineZero: 500, //TODO initiate at center of screen
+
+    boxes: [] as BoxModel[],
+    spacerHighestBox: {} as SpacerModel,
+    spacerPageEdge: {} as SpacerModel
   },
 
   mutations: {
     changeTimelineZero(state, value: number) {
-      state.timelineZero += value
+      state.timelineZero += value;
+    },
+    addBox(state, box: BoxModel) {
+      state.boxes.push(box);
+    },
+    setSpacerHighestBox(state, spacer: SpacerModel) {
+      state.spacerHighestBox = spacer;
+    },
+    setSpacerPageEdge(state, spacer: SpacerModel) {
+      state.spacerPageEdge = spacer;
     }
   },
 
---
+++ src/timeline/repositioner-instance.ts
@@ -0,0 +1,138 @@
+import store from "@/store";
+import Vue from "vue";
+
+export default class RepositionerInstance {
+  private timeline: HTMLElement;
+
+  private constructor() {
+    this.timeline = document.getElementById("timeline") as HTMLElement;
+  }
+
+  /**
+   * Moves all boxes away from the mouse pointer by the zoom factor.
+   * Then moves all boxes and the view into the positive space.
+   */
+  public zoomIn(zoomFactor: number, mousePosition: number) {
+    console.log("_________________________________________");
+    console.log("zoom factor " + zoomFactor + " mouse pos " + mousePosition);
+
+    this.repositionBoxes(zoomFactor, mousePosition);
+    this.repositionSpacerHighestBox(zoomFactor, mousePosition);
+
+    this.logPositions();
+
+    const distance = -store.state.boxes[0].positionLeft;
+
+    if (distance > 0) {
+      this.extendLeftSpace(distance);
+    }
+  }
+
+  /**
+   * Moves all boxes towards the mouse pointer by the zoom factor.
+   * Then moves all boxes and the view to the left by the amount of the minimum of the lowest box' left edge and the views' left edge.
+   */
+  public zoomOut(zoomFactor: number, mousePosition: number) {
+    console.log("_________________________________________");
+    console.log("zoom factor " + zoomFactor + " mouse pos " + mousePosition);
+
+    this.repositionBoxes(zoomFactor, mousePosition);
+    this.repositionSpacerHighestBox(zoomFactor, mousePosition);
+
+    this.logPositions();
+
+    const distance = Math.min(
+      store.state.boxes[0].positionLeft,
+      this.timeline.scrollLeft
+    );
+
+    if (distance > 0) {
+      this.cutLeftSpace(distance);
+    }
+  }
+
+  /**
+   * Repositions boxes by individual distances resulting from zoom and mouse position.
+   */
+  private repositionBoxes(zoomFactor: number, mousePosition: number) {
+    store.state.boxes.forEach(box => {
+      const positionCenter = box.positionLeft + box.width / 2;
+      const distance = (positionCenter - mousePosition) * zoomFactor;
+
+      box.positionLeft = mousePosition + distance - box.width / 2;
+    });
+  }
+
+  /**
+   * Repositions spacer according to highest box.
+   */
+  private repositionSpacerHighestBox(zoomFactor: number, mousePosition: number) {
+    const boxes = store.state.boxes;
+    const highestBox = boxes[boxes.length - 1];
+
+    store.state.spacerHighestBox.positionLeft =
+      highestBox.positionLeft + highestBox.width;
+  }
+
+  private cutLeftSpace(distance: number) {
+    console.log("cut space left by " + distance);
+
+    store.state.boxes.forEach(box => {
+      box.positionLeft -= distance;
+    });
+
+    store.state.spacerHighestBox.positionLeft -= distance;
+    store.state.spacerPageEdge.positionLeft =
+      this.timeline.scrollLeft +
+      this.timeline.clientWidth -
+      distance -
+      store.state.spacerPageEdge.width;
+
+    store.commit("changeTimelineZero", -distance);
+
+    this.timeline.scrollBy(-distance, 0);
+  }
+
+  private extendLeftSpace(distance: number) {
+    console.log("extend space left by " + distance);
+
+    store.state.boxes.forEach(box => {
+      box.positionLeft += distance;
+    });
+
+    store.state.spacerHighestBox.positionLeft += distance;
+    store.state.spacerPageEdge.positionLeft =
+      this.timeline.scrollLeft +
+      this.timeline.clientWidth +
+      distance -
+      store.state.spacerPageEdge.width;
+
+    store.commit("changeTimelineZero", distance);
+
+    Vue.nextTick(() => {
+      this.timeline.scrollBy(distance, 0);
+    });
+  }
+
+  private logPositions() {
+    store.state.boxes.forEach(box => {
+      console.log("box " + box.id + " Pos " + box.positionLeft);
+    });
+
+    console.log(
+      "spacerHighestBox Pos " +
+        store.state.spacerHighestBox.positionLeft +
+        " spacerPageEdge Pos " +
+        store.state.spacerPageEdge.positionLeft
+    );
+  }
+
+  private static instance: RepositionerInstance;
+  public static getInstance(): RepositionerInstance {
+    if (!RepositionerInstance.instance) {
+      RepositionerInstance.instance = new RepositionerInstance();
+    }
+
+    return RepositionerInstance.instance;
+  }
+}

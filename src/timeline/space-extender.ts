a525b85641a8052d97ef75f6b8c60187eaeb75f0
Valerian Backeberg <valerian@backebergs.de>
2020-10-11 15:39:12 +0200

    add space extender

---
+++ src/timeline/space-extender.ts
@@ -0,0 +1,76 @@
+import store from "@/store";
+import Vue from "vue";
+
+/**
+ * Listens to zoom transition ends, then extends space to the left if spacer
+ * is in the negative.
+ */
+export default class SpaceExtender {
+  constructor() {
+    const spacerLeftElement = document.getElementById("spacer-left");
+
+    if (!spacerLeftElement) {
+      console.error("SpacerLeft not found.");
+      return;
+    }
+
+    spacerLeftElement.addEventListener("transitionend", () => {
+      console.log("Transition ended");
+
+      const requiredLeftSpace = -store.state.SpacerLeft.positionLeft;
+
+      if (requiredLeftSpace > 0) {
+        for (let element of document.getElementsByClassName(
+          "box"
+        )) {
+          console.log("remove zoom-transition class");
+          element.classList.remove("zoom-transition");
+        }
+
+        this.extendLeftSpace(requiredLeftSpace);
+
+        Vue.nextTick(() => {
+          for (let element of document.getElementsByClassName(
+            "box"
+          )) {
+            console.log("re-add zoom-transition class");
+            element.classList.add("zoom-transition");
+          }
+        });
+      }
+    });
+  }
+
+  private extendLeftSpace(distance: number) {
+    console.log("extend space left by " + distance);
+
+    store.state.boxes.forEach(box => {
+      box.positionCenter += distance;
+    });
+
+    store.commit(
+      "setSpacerLeftPosition",
+      store.state.SpacerLeft.positionLeft + distance
+    );
+
+    store.commit(
+      "setSpacerRightPosition",
+      store.state.SpacerRight.positionLeft + distance
+    );
+
+    const timeline = document.getElementById("timeline") as HTMLElement;
+    store.commit(
+      "setSpacerPageEdgePosition",
+      timeline.scrollLeft +
+        timeline.clientWidth +
+        distance -
+        store.state.spacerPageEdge.width
+    );
+
+    store.commit("setTimelineZero", store.state.timelineZero + distance);
+
+    Vue.nextTick(() => {
+      timeline.scrollBy(distance, 0);
+    });
+  }
+}

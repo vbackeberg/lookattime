b22c1fb7160c845bd7b47d5c751dabe04a6ee316
Valerian Backeberg <valerian@backebergs.de>
2020-10-11 15:39:56 +0200

    make zoom transition class global

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -37,7 +37,4 @@ export default Vue.extend({
   border: 2px solid #000;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
-.zoom-transition {
-  transition: all var(--zoom-transition-duration) ease-out;
-}
 </style>

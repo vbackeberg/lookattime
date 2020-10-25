83231ef01a6ccf9f39f48d8640bd461cadce6b8b
Valerian Backeberg <valerian@backebergs.de>
2020-10-25 20:59:09 +0100

    add date component

---
+++ src/components/timeline-element/Date.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    id="date"
+    class="zoom-transition"
+    v-bind:style="{ left: positionLeft + 'px' }"
+  ></div>
+</template>
+
+<script lang="ts">
+import store from "@/store";
+import Vue from "vue";
+export default Vue.extend({
+  name: "Date",
+
+  props: {
+    boxId: Number
+  },
+
+  computed: {
+    positionLeft(): number {
+      const box = store.state.boxes[this.boxId];
+
+      return box.positionCenter - box.width / 2;
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss">
+#connector {
+  height: 100%;
+  position: absolute;
+}
+</style>

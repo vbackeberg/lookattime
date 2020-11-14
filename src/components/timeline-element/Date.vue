7bc9ef8dc4abb81ada0f117de799b20d7afac692
Valerian Backeberg <valerian@backebergs.de>
2020-11-14 19:55:42 +0100

    For date component, add hide function with animation

--- src/components/timeline-element/Date.vue
+++ src/components/timeline-element/Date.vue
@@ -3,12 +3,17 @@
     class="date zoom-transition zoomable"
     v-bind:style="{ left: positionLeft + 'px', width: width + 'px' }"
   >
-    {{ this.box.date }}
+    <transition>
+      <div v-if="!hide" class="inner">
+        {{ this.box.date }}
+      </div>
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
 import BoxModel from "@/models/box-model";
+import store from "@/store";
 import Vue from "vue";
 export default Vue.extend({
   name: "Date",
@@ -24,6 +29,46 @@ export default Vue.extend({
 
     width(): number {
       return this.box.width;
+    },
+
+    boxIndex(): number {
+      return store.state.boxes.findIndex(box => box.id === this.box.id);
+    },
+
+    hide(): boolean {
+      if (this.hideRecursive(this.boxIndex, -1)) return true;
+      if (this.hideRecursive(this.boxIndex, 1)) return true;
+      return false;
+    }
+  },
+
+  methods: {
+    hideRecursive(currentBoxIndex: number, indexChange: number): boolean {
+      const neighborBox = store.state.boxes[currentBoxIndex + indexChange];
+
+      if (!neighborBox) {
+        return false;
+      }
+
+      const collision =
+        (this.box.positionCenter +
+          (indexChange * BoxModel.collapsedWidth) / 2 -
+          (neighborBox.positionCenter +
+            (-indexChange * BoxModel.collapsedWidth) / 2)) *
+          indexChange >
+        0;
+
+      if (!collision) {
+        return false;
+      }
+
+      const hasPrecedence = this.box.importance > neighborBox.importance;
+
+      if (!hasPrecedence) {
+        return true;
+      }
+
+      return this.hideRecursive(currentBoxIndex + indexChange, indexChange);
     }
   }
 });
@@ -35,5 +80,18 @@ export default Vue.extend({
   vertical-align: top;
 
   text-align: center;
+}
+
+.v-enter-active {
+  transition: all 300ms cubic-bezier(0.22, 0.61, 0.36, 1);
+}
+
+.v-leave-active {
+  transition: all 300ms cubic-bezier(0.22, 0.61, 0.36, 1);
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
 }
 </style>

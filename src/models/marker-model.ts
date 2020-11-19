dd47802d8c82429cd437c010b5c017832dcb2722
Valerian Backeberg <valerian@backebergs.de>
2020-11-19 17:39:48 +0100

    add todo

--- src/models/marker-model.ts
+++ src/models/marker-model.ts
@@ -2,6 +2,7 @@ export default class MarkerModel {
   positionCenter: number;
   id: number;
 
+  //TODO: Add relative Position (date)
   constructor(positionCenter: number, id: number) {
     this.positionCenter = positionCenter;
     this.id = id;

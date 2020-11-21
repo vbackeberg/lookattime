2da6e836fa5ad1a4dd030563bd26175033cad693
Valerian Backeberg <valerian@backebergs.de>
2020-11-21 18:47:46 +0100

    in box creator call view focuser

--- src/timeline/box-creator.ts
+++ src/timeline/box-creator.ts
@@ -3,24 +3,25 @@ import TimeMarkerModel from "@/models/marker-model";
 import store from "@/store";
 import Vue from "vue";
 import SpaceExtender from "./space-extender";
+import ViewFocuser from "./view-focuser";
 
 export default class BoxCreator {
   private timelineElement: HTMLElement;
   private spacerLeftElement: HTMLElement;
+  private viewFocuser: ViewFocuser;
 
   constructor() {
     this.timelineElement = document.getElementById("timeline") as HTMLElement;
     this.spacerLeftElement = document.getElementById(
       "spacer-left"
     ) as HTMLElement;
+    this.viewFocuser = ViewFocuser.Instance;
   }
 
   public async addBox(box: BoxModel) {
     this.spacerLeftElement.classList.remove("zoom-transition");
 
     store.commit("addBox", box);
-
-    this.addTimeMarkers();
 
     await Vue.nextTick();
 
@@ -31,22 +32,21 @@ export default class BoxCreator {
 
     this.spacerLeftElement.classList.add("zoom-transition");
 
-    const position = box.positionCenter - this.timelineElement.clientWidth / 2;
-    this.timelineElement.scrollTo({
-      left: position,
-      behavior: "smooth"
-    });
+    this.focusView(box);
   }
-  addTimeMarkers() {
-    // TOOD: If new box is new heighest or lowest,
-    // fill space up to new box with new time markers.
-    
-    // For highest: Determine distance highest to second highest
-    
-    // extend time markers with new markers each with distance of timeMarkerDistance
-    // to the previous one:
-    // (positionCenter = previousPositionCenter + timeMarkerDistance)
 
+  private focusView(box: BoxModel) {
+    if (store.state.boxes.length === 2) {
+      this.viewFocuser.focusOnRange(
+        store.state.boxes[0].date,
+        store.state.boxes[1].date
+      );
+    } else {
+      this.viewFocuser.focusOnBox(box);
+    }
+  }
+
+ 
 
   private static instance: BoxCreator;
   public static get Instance() {

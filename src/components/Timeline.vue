9add04c88e76d6d4ca4df2d91a6cb21f6e874582
Valerian Backeberg <valerian@backebergs.de>
2020-11-22 10:22:48 +0100

    connector and horizontal line shadow

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -159,9 +159,10 @@ export default Vue.extend({
 }
 
 #horizontal-line {
-  flex: 0 0 10px;
+  flex: 0 0 8px;
 
   background-color: #000;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
 #buffer-bottom-area {
--- src/components/timeline-element/Connector.vue
+++ src/components/timeline-element/Connector.vue
@@ -19,5 +19,6 @@ export default Vue.extend({
 .connector {
   height: 100%;
   background-color: #000;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 </style>

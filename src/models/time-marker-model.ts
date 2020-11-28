893b0666b0ef299a191d6720165275a871ff64f7
Valerian Backeberg <valerian@backebergs.de>
2020-11-28 12:24:12 +0100

    In time marker model add depth field

--- src/models/time-marker-model.ts
+++ src/models/time-marker-model.ts
@@ -2,9 +2,11 @@ export default class TimeMarkerModel {
   positionCenter: number;
   id: string;
   date: number;
-  constructor(positionCenter: number, id: string, date: number) {
+  depth: number;
+  constructor(positionCenter: number, id: string, date: number, depth: number) {
     this.positionCenter = positionCenter;
     this.id = id;
     this.date = date;
+    this.depth = depth;
   }
 }

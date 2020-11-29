dbff13488f4eb69a9efb0654b411fe5239e1cbe9
Valerian Backeberg <valerian@backebergs.de>
2020-11-29 13:44:58 +0100

    rename

--- src/timeline/position-translator.ts
+++ src/timeline/position-translator.ts
@@ -1,6 +1,6 @@
 import store from "@/store";
 
-export default class PositionMapper {
+export default class PositionTranslator {
   public static toAbsolutePosition(relativePosition: number): number {
     return store.state.timelineZero + relativePosition * store.state.zoomLevel;
   }

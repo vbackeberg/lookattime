3bcf59cd82c39f6fe2afa6cc8d9630f4bf7ef586
Valerian Backeberg <valerian@backebergs.de>
2020-11-29 11:02:45 +0100

    For time marker, add enter and leave animation

--- src/components/TimeMarker.vue
+++ src/components/TimeMarker.vue
@@ -1,10 +1,12 @@
 <template>
-  <div
-    class="marker zoomable"
-    v-bind:style="{ left: positionLeft + 'px', width: width + 'px' }"
-  >
-    {{ date }}
-  </div>
+  <transition>
+    <div
+      class="marker zoomable"
+      v-bind:style="{ left: positionLeft + 'px', width: width + 'px' }"
+    >
+      {{ date }}
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -38,5 +40,12 @@ export default Vue.extend({
   position: absolute;
   height: 16px;
   background-color: brown;
+}
+.v-enter-active {
+  transition: all 300ms cubic-bezier(0.22, 0.61, 0.36, 1);
+}
+
+.v-leave-active {
+  transition: all 300ms cubic-bezier(0.22, 0.61, 0.36, 1);
 }
 </style>

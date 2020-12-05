60233b838363fca1d8a7411ba5ef62ac8fc57bb3
Valerian Backeberg <valerian@backebergs.de>
2020-12-05 21:21:47 +0100

    time marker creator add remove left and remove right

--- src/timeline/time-marker/time-marker-creator.ts
+++ src/timeline/time-marker/time-marker-creator.ts
@@ -161,6 +161,37 @@ export default class TimeMarkerCreator {
     return markers;
   }
 
+  public removeMarkersLeft() {
+    const leftEdge = Math.min(0, store.getters.spacerLeft.positionLeft);
+
+    const index = store.state.timeMarkers.findIndex(
+      marker => marker.positionCenter > leftEdge
+    );
+
+    console.log("Remove markers below index: " + index);
+
+    if (index > 0) {
+      store.commit("setTimeMarkers", store.state.timeMarkers.slice(index));
+    }
+  }
+
+  public removeMarkersRight() {
+    const rightEdge = Math.max(
+      store.getters.spacerRight.positionLeft + store.getters.spacerRight.width,
+      store.state.spacerPageEdge.positionLeft + store.state.spacerPageEdge.width
+    );
+
+    const index = store.state.timeMarkers.findIndex(
+      marker => marker.positionCenter > rightEdge
+    );
+
+    console.log("Remove markers above index: " + index);
+
+    if (index > -1) {
+      store.commit("setTimeMarkers", store.state.timeMarkers.slice(0, index));
+    }
+  }
+
   private countDecimals(number: number) {
     if (!isFinite(number)) return 0;
 

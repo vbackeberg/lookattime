2bd9179f7d4c17e2d0cd8c27444c57434dbef532
Valerian Backeberg <valerian@backebergs.de>
2020-12-05 21:21:27 +0100

    Remove comments

--- src/timeline/time-marker/time-marker-creator.ts
+++ src/timeline/time-marker/time-marker-creator.ts
@@ -94,12 +94,10 @@ export default class TimeMarkerCreator {
     relativeRightEdge: number,
     power: number
   ): TimeMarkerModel {
-    let secondMarkerDate = firstMarkerDate - power; // 1. 1900 - 100 / 10^0 = 1800    | 2. 1900 - 100 / 10^1 = 1890
+    let secondMarkerDate = firstMarkerDate - power;
     if (secondMarkerDate < relativeLeftEdge) {
-      // 1. 1800 < 1899   | 2. 1890 < 1899
-      secondMarkerDate = firstMarkerDate + power; // 1. 1900 + 100 / 10^0= 2000   | 2. 1900 + 100 / 10^1 = 1910
+      secondMarkerDate = firstMarkerDate + power;
       if (secondMarkerDate > relativeRightEdge) {
-        // 1. 2000 > 1950   | 2. 1910 !> 1950
         return this.createSecondMarker(
           firstMarkerDate,
           relativeLeftEdge,
@@ -113,18 +111,16 @@ export default class TimeMarkerCreator {
       uuid(),
       secondMarkerDate,
       power
-    ); // 2. return 1910
+    );
   }
 
   public createMarkersLeft(
-    //TODO: Refactor: fillSpaceWithMarkers to the left. Reduce parameters accordingly.
     leftmostMarker: TimeMarkerModel,
     relativeLeftEdge: number,
     depth: number
   ): TimeMarkerModel[] {
-    // first marker, 1899, 10
-    const distanceToEdge = leftmostMarker.date - relativeLeftEdge; // 1900 - 1899 = 1
-    const numberOfMarkers = Math.floor(distanceToEdge / depth); // floor (1 / 10) = 0
+    const distanceToEdge = leftmostMarker.date - relativeLeftEdge;
+    const numberOfMarkers = Math.floor(distanceToEdge / depth);
     const absoluteMarkerDistance = depth * store.state.zoomLevel;
     const markers = [] as TimeMarkerModel[];
 
@@ -134,7 +130,7 @@ export default class TimeMarkerCreator {
           leftmostMarker.positionCenter - absoluteMarkerDistance * i,
           uuid(),
           leftmostMarker.date - depth * i,
-          depth // relative marker distance = depth = power
+          depth
         )
       );
     }
@@ -147,9 +143,8 @@ export default class TimeMarkerCreator {
     relativeRightEdge: number,
     depth: number
   ): TimeMarkerModel[] {
-    // second marker, 1950, 10
-    const distanceToEdge = relativeRightEdge - rightmostMarker.date; // 1950 - 1910 = 40
-    const numberOfMarkers = Math.floor(distanceToEdge / depth); // floor (40 / 10) = 4
+    const distanceToEdge = relativeRightEdge - rightmostMarker.date;
+    const numberOfMarkers = Math.floor(distanceToEdge / depth);
     const absoluteMarkerDistance = depth * store.state.zoomLevel;
     const markers = [] as TimeMarkerModel[];
 
@@ -159,7 +154,7 @@ export default class TimeMarkerCreator {
           rightmostMarker.positionCenter + absoluteMarkerDistance * i,
           uuid(),
           rightmostMarker.date + depth * i,
-          depth // relative marker distance = depth = power
+          depth
         )
       );
     }

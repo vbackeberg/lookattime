291c7e0f301476ec1daaefaea3fe4cc2b0cbdebe
Valerian Backeberg <valerian@backebergs.de>
2020-12-19 17:43:38 +0100

    Create box form

---
+++ src/components/create-box-form.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog v-model="show" max-width="290">
+    <v-card>
+      <v-card-title class="headline">
+        Use Google's location service?
+      </v-card-title>
+      <v-card-text
+        >Let Google help apps determine location. This means sending anonymous
+        location data to Google, even when no apps are running.</v-card-text
+      >
+      <v-card-actions>
+        <v-btn color="primary" text @click.stop="show = false">
+          Disagree
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "CreateBoxForm",
+
+  props: {
+    value: Boolean
+  },
+
+  computed: {
+    show: {
+      get(): boolean {
+        return this.value;
+      },
+      set(value: boolean) {
+        this.$emit("input", value);
+      }
+    }
+  }
+});
+</script>
+
+<style lang="scss"></style>

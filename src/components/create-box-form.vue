092619a800f0d28ac79c34ad9fcd203761f0a49c
Valerian Backeberg <valerian@backebergs.de>
2020-12-19 18:11:58 +0100

    form wip

--- src/components/create-box-form.vue
+++ src/components/create-box-form.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-dialog v-model="show" max-width="290">
+  <v-dialog v-model="show" persistent max-width="600">
     <v-card>
       <v-card-title class="headline">
-        Use Google's location service?
+        Create a new event
       </v-card-title>
-      <v-card-text
-        >Let Google help apps determine location. This means sending anonymous
-        location data to Google, even when no apps are running.</v-card-text
-      >
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field label="Title" required />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-icon />
+            </v-col>
+            <v-col cols="12"> <v-text-field label="Text" required /> </v-col
+          ></v-row>
+        </v-container>
+      </v-card-text>
       <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" text @click.stop="back()">
+          Back
+        </v-btn>
         <v-btn color="primary" text @click.stop="show = false">
-          Disagree
+          Create
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -19,6 +32,9 @@
 
 <script lang="ts">
 import Vue from "vue";
+import BoxCreator from "@/timeline/box-creator";
+import BoxModel from "@/models/box-model";
+
 export default Vue.extend({
   name: "CreateBoxForm",
 
@@ -34,6 +50,23 @@ export default Vue.extend({
       set(value: boolean) {
         this.$emit("input", value);
       }
+    }
+  },
+
+  methods: {
+    create() {
+      this.addBox();
+      this.show = false;
+    },
+
+    addBox() {
+      const box = new BoxModel();
+
+      BoxCreator.Instance.addBox(box);
+    },
+
+    back() {
+      this.show = false;
     }
   }
 });

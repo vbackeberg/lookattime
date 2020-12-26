2dc98eebc158c66560fd272b58a1888e23ff525e
Valerian Backeberg <valerian@backebergs.de>
2020-12-26 16:53:43 +0100

    return 2 dummy time events from api with id and imageId

--- api/get-timeline/index.ts
+++ api/get-timeline/index.ts
@@ -7,16 +7,28 @@ const httpTrigger: AzureFunction = async function (
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
 
-  const response = {
-    title: "Title",
-    date: 1510,
-    importance: 20,
-    text: "Text Text Text Text Text Text Text Text Text Text ",
-  };
+  const response = [
+    {
+      id: 1,
+      title: "Title",
+      date: 1510,
+      importance: 20,
+      text: "Text Text Text Text Text Text Text Text Text Text ",
+      imageIds: [4, 2],
+    },
+    {
+      id: 2,
+      title: "Event 2",
+      date: 1510,
+      importance: 20,
+      text: "Text Text Text Text Text Text Text Text Text Text ",
+      imageIds: [6, 11],
+    },
+  ];
 
   const responseMessage = name
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : [response, response];
+    : response;
 
   context.res = {
     // status: 200, /* Defaults to 200 */

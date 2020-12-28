665d2c88358ac2264feeed139169a4543f2909f2
Valerian Backeberg <valerian@backebergs.de>
2020-12-28 19:08:06 +0100

    reorder fields

--- web/src/models/time-marker-model.ts
+++ web/src/models/time-marker-model.ts
@@ -1,10 +1,10 @@
 export default class TimeMarkerModel {
+  static width = 4;
   positionCenter: number;
   id: string;
   date: number;
   depth: number;
   show = false;
-  static width = 4;
   constructor(positionCenter: number, id: string, date: number, depth: number) {
     this.positionCenter = positionCenter;
     this.id = id;

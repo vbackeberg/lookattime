bffcbd073cd63eafe5cfec3181e11a1504bac436
Valerian Backeberg <valerian@backebergs.de>
2020-12-31 17:02:23 +0100

    In get-timeline, first implementation of db query.

--- api/get-timeline/index.ts
+++ api/get-timeline/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Connection, Request } from "tedious";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -7,33 +8,68 @@ const httpTrigger: AzureFunction = async function (
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
 
-  const response = [
-    {
-      id: 1,
-      title: "Title",
-      date: 1510,
-      importance: 20,
-      text: "Text Text Text Text Text Text Text Text Text Text ",
-      imageIds: [4, 2],
+  const config = {
+    authentication: {
+      options: {
+        userName: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+      },
+      type: "default",
     },
-    {
-      id: 2,
-      title: "Event 2",
-      date: 1513,
-      importance: 21,
-      text: "Text Text Text Text Text Text Text Text Text Text ",
-      imageIds: [6, 11],
+    server: process.env.DB_SERVER,
+    options: {
+      database: process.env.DB_DATABASE,
+      encrypt: true,
+      validateBulkLoadParameters: true,
     },
-  ];
+  };
+
+  const connection = new Connection(config);
+  connection.connect();
+
+  connection.on("connect", function (err) {
+    if (err) {
+      console.log("Error: ", err);
+    }
+    console.log("connected");
+
+    executeQuery(connection);
+  });
 
   const responseMessage = name
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : response;
+    : "response";
 
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: responseMessage,
   };
 };
+
+function executeQuery(connection) {
+  const request = new Request("SELECT * from timelines", function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  let result = "";
+  request.on("row", function (columns) {
+    columns.forEach(function (column) {
+      if (column.value === null) {
+        console.log("NULL");
+      } else {
+        result += column.value + " ";
+      }
+    });
+    console.log(result);
+    result = "";
+  });
+
+  request.on("done", function (rowCount, more) {
+    console.log(rowCount + " rows returned");
+  });
+  connection.execSql(request);
+}
 
 export default httpTrigger;

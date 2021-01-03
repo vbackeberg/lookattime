d8be4bdfe96613f97b7b4f07df2a8662350526c3
Valerian Backeberg <valerian@backebergs.de>
2021-01-03 21:57:02 +0100

    add todo

--- web/src/api/time-event-response-mapper.ts
+++ web/src/api/time-event-response-mapper.ts
@@ -2,6 +2,7 @@ import TimeEventModel from "@/models/time-event-model";
 import TimeEventResponse from "@/api/time-event-response";
 import PositionTranslator from "@/timeline/position-translator";
 
+//TODO Move into time event dir
 export default class TimeEventResponseMapper {
   public static map(response: TimeEventResponse): TimeEventModel {
     return new TimeEventModel(

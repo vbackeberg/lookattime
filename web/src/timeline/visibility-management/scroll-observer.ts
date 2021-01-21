4b9b8ffa5c3dd832da7a6ae283042418f6e9fe19
Valerian Backeberg <valerian@backebergs.de>
2021-01-21 19:10:19 +0100

    swap functions

--- web/src/timeline/visibility-management/scroll-observer.ts
+++ web/src/timeline/visibility-management/scroll-observer.ts
@@ -10,23 +10,22 @@ export default class ScrollObserver {
   private constructor() {
     this.timelineElement = document.getElementById("timeline") as HTMLElement;
     this.timelineElement.addEventListener("scroll", () => {
-
       const viewportLeftEdge = this.timelineElement.scrollLeft;
       const viewportRightEdge =
         viewportLeftEdge + this.timelineElement.clientWidth;
-
-      if (store.state.timeMarkers.length > 0) {
-        TimeMarkerHider.changeTimeMarkerVisibility(
-          viewportLeftEdge,
-          viewportRightEdge
-        );
-      }
 
       if (store.state.timeEvents.length > 0) {
         TimeEventHider.changeTimeEventVisibility(
           viewportLeftEdge,
           viewportRightEdge
         );
+
+        if (store.state.timeMarkers.length > 0) {
+          TimeMarkerHider.changeTimeMarkerVisibility(
+            viewportLeftEdge,
+            viewportRightEdge
+          );
+        }
       }
     });
   }

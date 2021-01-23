3ab2e83323035be67c38ea077bca941bbb64a54c
Valerian Backeberg <valerian@backebergs.de>
2021-01-23 22:14:28 +0100

    API add function delete timeline

---
+++ api/delete-timeline/index.ts
@@ -0,0 +1,39 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { validate as validUuid } from "uuid";
+const sql = require("mssql");
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger function processed a request.");
+
+  const id = req.query.id;
+  const userId = req.query.userId;
+
+  if (!validRequest(id, userId)) {
+    context.res = {
+      status: 400,
+    };
+  } else {
+    try {
+      await sql.connect(
+        `mssql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}?encrypt=true`
+      );
+
+      const result = await sql.query(
+        `delete from timelines where id = '${id}' and userId = '${userId}'`
+      );
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
+function validRequest(id: string, userId: string) {
+  return validUuid(id) && validUuid(userId);
+}
+
+export default httpTrigger;

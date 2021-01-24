ee95432087403f302f23e9a775bb26541abab640
Valerian Backeberg <valerian@backebergs.de>
2021-01-24 16:47:40 +0100

    format

--- api/get-timeline/index.ts
+++ api/get-timeline/index.ts
@@ -1,11 +1,14 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { validate as validUuid } from "uuid";
 const sql = require("mssql");
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    
-    const id = req.query.id;
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger function processed a request.");
+
+  const id = req.query.id;
 
   if (!validUuid(id)) {
     context.res = {
@@ -30,7 +33,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
       console.log(e);
     }
   }
-
 };
 
 export default httpTrigger;

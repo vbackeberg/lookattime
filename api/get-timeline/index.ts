615cf59fe3aa0266b4ee63e8e1b27e80c4cc7d86
Valerian Backeberg <valerian@backebergs.de>
2021-01-24 10:24:00 +0100

    API Add get timeline function

---
+++ api/get-timeline/index.ts
@@ -0,0 +1,36 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { validate as validUuid } from "uuid";
+const sql = require("mssql");
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    
+    const id = req.query.id;
+
+  if (!validUuid(id)) {
+    context.res = {
+      status: 400,
+    };
+  } else {
+    try {
+      await sql.connect(
+        `mssql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}?encrypt=true`
+      );
+
+      const result = await sql.query(
+        `select id, title from timelines where id = '${id}';`
+      );
+
+      context.res = {
+        body: result.recordset,
+      };
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+};
+
+export default httpTrigger;

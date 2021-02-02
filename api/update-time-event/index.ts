7cf2280a39504bccf07de296fb698cb986c83490
Valerian Backeberg <valerian@backebergs.de>
2021-02-02 21:27:52 +0100

    API add update time event endpoint

---
+++ api/update-time-event/index.ts
@@ -0,0 +1,44 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { validate as validUuid } from "uuid";
+import TimeEventRequest from "../shared/time-event-request";
+const sql = require("mssql");
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger function processed a request.");
+
+  const timeEventRequest = req.body as TimeEventRequest;
+
+  if (!validRequest(timeEventRequest)) {
+    context.res = {
+      status: 400,
+    };
+  } else {
+    try {
+      await sql.connect(
+        `mssql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}?encrypt=true`
+      );
+
+      const result = await sql.query(
+        `update timeEvents set id = '${timeEventRequest.id}', timelineId = '${timeEventRequest.timelineId}', title = '${timeEventRequest.title}', textValue = '${timeEventRequest.textValue}', dateValue = ${timeEventRequest.dateValue}, importanceValue = ${timeEventRequest.importanceValue};`
+      );
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
+function validRequest(timeEventRequest: TimeEventRequest): boolean {
+  return (
+    validUuid(timeEventRequest.id) &&
+    validUuid(timeEventRequest.timelineId) &&
+    !isNaN(timeEventRequest.dateValue) &&
+    !isNaN(timeEventRequest.importanceValue)
+  );
+}
+
+export default httpTrigger;

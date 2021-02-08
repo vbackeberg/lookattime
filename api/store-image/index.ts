95587fe75ac35bdacd6ca930bd37a497409db6d1
Valerian Backeberg <valerian@backebergs.de>
2021-02-08 19:01:37 +0100

    API add store image endpoint that stores imageId for given time event and image data in blob storage.

--- api/store-image/index.ts
+++ api/store-image/index.ts
@@ -1,8 +1,9 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { validateBufferMIMEType } from "validate-image-type";
 import { BlockBlobClient } from "@azure/storage-blob";
-import { v4 as uuid } from "uuid";
+import { validate as validUuid, v4 as uuid } from "uuid";
 import * as multipart from "parse-multipart";
+const sql = require("mssql");
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -10,32 +11,45 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   context.log("HTTP trigger function processed a request.");
 
-  const imageData = multipart.Parse(
-    Buffer.from(req.body),
-    multipart.getBoundary(req.headers["content-type"])
-  )[0].data as Buffer;
+  const imageData = getImageData(req);
 
-  if (!validImage(imageData)) {
+  if (!validQueryParameters(req.query) || !validImage(imageData)) {
     context.res = {
       status: 400,
     };
   } else {
-    const imageId = uuid();
+    const imageId = req.query.imageId;
+    const timeEventId = req.query.timeEventId;
+    const timelineId = req.query.timelineId;
+    const userId = req.query.userId;
 
-    const blockBlobClient = new BlockBlobClient(
-      process.env.AzureWebJobsStorageLookattime,
-      process.env.AzureWebJobsStorageLookattime_ContainerName,
-      imageId
-    );
+    try {
+      const storeImageIdTask = storeImageId(
+        imageId,
+        timeEventId,
+        timelineId,
+        userId
+      );
+      const storeImageTask = storeImage(imageId, imageData);
 
-    const response = await blockBlobClient.upload(imageData, imageData.length);
+      await storeImageIdTask;
+      const response = await storeImageTask;
 
-    context.res = {
-      body: { imageId: imageId },
-      status: response._response.status,
-    };
+      context.res = {
+        status: response._response.status,
+      };
+    } catch (e) {
+      console.warn(e);
+    }
   }
 };
+
+function getImageData(req: HttpRequest): Buffer {
+  return multipart.Parse(
+    Buffer.from(req.body),
+    multipart.getBoundary(req.headers["content-type"])
+  )[0].data;
+}
 function validImage(imageData: Buffer): boolean {
   const result = validateBufferMIMEType(imageData, {
     allowMimeTypes: ["image/jpeg", "image/gif", "image/png", "image/svg+xml"],
@@ -46,6 +60,40 @@ function validImage(imageData: Buffer): boolean {
   }
 
   return result.ok;
+}
+
+function validQueryParameters(query: any): boolean {
+  return (
+    validUuid(query.imageId) &&
+    validUuid(query.timeEventId) &&
+    validUuid(query.timelineId) &&
+    validUuid(query.userId)
+  );
+}
+
+async function storeImageId(
+  imageId: string,
+  timeEventId: string,
+  timelineId: string,
+  userId: string
+) {
+  await sql.connect(
+    `mssql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}?encrypt=true`
+  );
+
+  return await sql.query(
+    `if exists ( select * from timeEvents where id = '${timeEventId}' and timelineId in (select id from timelines where id = '${timelineId}' and userId = '${userId}')) insert into images values ('${imageId}', '${timeEventId}');`
+  );
+}
+
+async function storeImage(imageId: string, imageData: Buffer) {
+  const blockBlobClient = new BlockBlobClient(
+    process.env.AzureWebJobsStorageLookattime,
+    process.env.AzureWebJobsStorageLookattime_ContainerName,
+    imageId
+  );
+
+  return await blockBlobClient.upload(imageData, imageData.length);
 }
 
 export default httpTrigger;

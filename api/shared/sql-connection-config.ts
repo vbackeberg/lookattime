4f2656a6f9ffd85e138015e5b34a15c56497e06f
Valerian Backeberg <valerian@backebergs.de>
2021-02-13 11:16:36 +0100

    API Add ms sql connection config

---
+++ api/shared/sql-connection-config.ts
@@ -0,0 +1,11 @@
+export const sqlConnectionConfig = {
+  server: process.env.DB_SERVER,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  options: {
+    encrypt: true,
+    enableArithAbort: true,
+    lowerCaseGuids: true
+  },
+};

5c5046f80c88ec0e972406ec72f43ac4bf51ef8b
Valerian Backeberg <valerian@backebergs.de>
2021-02-14 16:36:15 +0100

    In Time Marker Distance Watcher, remove on zoom in and out.
    Instead, always add and remove time markers after each other as a brute force method to solve time marker creation problems when initating.

--- web/src/timeline/time-marker-management/time-marker-distance-watcher.ts
+++ web/src/timeline/time-marker-management/time-marker-distance-watcher.ts
@@ -21,34 +21,14 @@ export default class TimeMarkerDistanceWatcher {
       return;
     }
 
-    if (newDistance > oldDistance) {
-      this.onZoomIn(newDistance);
-    } else if (newDistance < oldDistance) {
-      this.onZoomOut(newDistance);
-    }
-  }
-  private onZoomIn(newDistance: number) {
-    if (store.state.timeMarkers.length > 2) {
-      this.timeMarkerRemover.removeMarkersLeft();
-      this.timeMarkerRemover.removeMarkersRight();
-    }
-
     if (newDistance > Constants.MAX_DISTANCE) {
       if (store.state.timeMarkerDepth === 1) {
         return;
         // TODO: If depth < 1 change time system to the next lower system.
         // Ex.: From years to months. From seconds to milliseconds
       }
-
       this.timeMarkerCreator.addMarkersBetween();
-    }
-  }
-
-  private onZoomOut(newDistance: number) {
-    this.timeMarkerCreator.addMarkersLeft();
-    this.timeMarkerCreator.addMarkersRight();
-
-    if (newDistance < Constants.MIN_DISTANCE) {
+    } else if (newDistance < Constants.MIN_DISTANCE) {
       if (store.state.timeMarkerDepth === Number.MAX_SAFE_INTEGER) {
         return;
 
@@ -56,8 +36,14 @@ export default class TimeMarkerDistanceWatcher {
         // Ex.: From months to years. From milliseconds to seconds.
         // Or throw exception
       }
-
       this.timeMarkerRemover.removeMarkersLowestDepth();
+    }
+
+    this.timeMarkerCreator.addMarkersLeft();
+    this.timeMarkerCreator.addMarkersRight();
+    if (store.state.timeMarkers.length > 2) {
+      this.timeMarkerRemover.removeMarkersLeft();
+      this.timeMarkerRemover.removeMarkersRight();
     }
   }
 

c09bdbf0f54750899190b879460fc050666e6fcc
Valerian Backeberg <valerian@backebergs.de>
2021-02-20 16:11:36 +0100

    time marker creator tests

--- web/tests/unit/time-marker-creator.spec.ts
+++ web/tests/unit/time-marker-creator.spec.ts
@@ -4,10 +4,26 @@ import TimeMarkerCreator from "../../src/timeline/time-marker-management/time-ma
 describe("TimeMarkerCreator", () => {
   const timeMarkerCreator = TimeMarkerCreator.Instance;
   describe("createFirstMarker", () => {
-    it("find correct date", () => {
+    it("find correct date when both positive", () => {
       const expectedMarkerDate = 1000;
 
       const actualMarkerDate = timeMarkerCreator.createFirstMarker(600, 1200).date;
+
+      expect(actualMarkerDate).toEqual(expectedMarkerDate);
+    });
+    
+    it("find correct date when negative and positive", () => {
+      const expectedMarkerDate = 0;
+
+      const actualMarkerDate = timeMarkerCreator.createFirstMarker(-10, 1200).date;
+
+      expect(actualMarkerDate).toEqual(expectedMarkerDate);
+    });
+    
+    it("find correct date when both negative", () => {
+      const expectedMarkerDate = -10000;
+
+      const actualMarkerDate = timeMarkerCreator.createFirstMarker(-12000, -120).date;
 
       expect(actualMarkerDate).toEqual(expectedMarkerDate);
     });

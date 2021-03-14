0b539fda2dcf43e4e94987a3c5a78e3f6f2ea79c
Valerian Backeberg <valerian@backebergs.de>
2021-03-14 10:21:19 +0100

    When store images fails, delete time event.

--- web/src/components/timeline/create-time-event-form.vue
+++ web/src/components/timeline/create-time-event-form.vue
@@ -179,6 +179,7 @@ export default Vue.extend({
         store.commit("updateTimeEvent", timeEvent);
       } catch (e) {
         console.warn(e);
+        store.dispatch("deleteTimeEvent", timeEvent);
       }
     }
   }

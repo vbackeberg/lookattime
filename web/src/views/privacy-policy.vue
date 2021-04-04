6de4db1475dd2e4e2ad17e60e87848502a3b1ffa
Valerian Backeberg <valerian@backebergs.de>
2021-04-04 18:49:21 +0200

    privacy policy

---
+++ web/src/views/privacy-policy.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <div class="privacy-policy">
+      <h1>Privacy policy</h1>
+      <p>
+        <strong>Data we store:</strong>
+        user id: Your user id is used to retrieve your timelines and associated
+        data after you close your session.
+        <br />
+        selected timeline id: The timeline that you have currently opened. This
+        allows the site to open the correct timeline if you refresh or close
+        your browser window.
+
+        <br /><br /><strong>Retention Period:</strong>
+        The aforementioned data is retained indefinitely until you delete it
+        from your local storage.
+
+        <br /><br /><strong>Your rights pertaining your data:</strong>
+        You can request us to erase your user id and all the data associated to
+        it. Please send your request to contact.lookattime@gmail.com.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+  position: absolute;
+  margin: 16px;
+  overflow-y: scroll;
+}
+</style>

87f3a66eaf2e9bfd06c060fdb1a7b63627cb69cc
Valerian Backeberg <valerian@backebergs.de>
2021-04-06 11:25:22 +0200

    privacy policy remove obsolete div

--- web/src/views/privacy-policy.vue
+++ web/src/views/privacy-policy.vue
@@ -1,146 +1,144 @@
 <template>
   <div class="wrapper">
-    <div class="privacy-policy">
-      <h1>Privacy policy</h1>
-      Look at Time is a personal website with no commercial purpose.<br />
-      This privacy policy will explain how we use th data we collect from you
-      when you use our website.<br />
-      <br />
+    <h1>Privacy policy</h1>
+    Look at Time is a personal website with no commercial purpose.<br />
+    This privacy policy will explain how we use the data we collect from you
+    when you use our website.<br />
+    <br />
 
-      <h2>Topics:</h2>
-      Specific terms used in this policy<br />
-      What data do we collect?<br />
-      How do we collect your data?<br />
-      How will we use your data?<br />
-      How do we store your data?<br />
-      Marketing<br />
-      What are your data protection rights?<br />
-      Do we use cookies?<br />
-      Do we use local storage?<br />
-      How do we use local storage?<br />
-      How to manage local storage data?<br />
-      Changes to our privacy policy<br />
-      How to contact us
-      <br /><br />
+    <h2>Topics:</h2>
+    - Specific terms used in this policy<br />
+    - What data do we collect?<br />
+    - How do we collect your data?<br />
+    - How will we use your data?<br />
+    - How do we store your data?<br />
+    - Marketing<br />
+    - What are your data protection rights?<br />
+    - Do we use cookies?<br />
+    - Do we use local storage?<br />
+    - How do we use local storage?<br />
+    - How to manage local storage data?<br />
+    - Changes to our privacy policy<br />
+    - How to contact us?
+    <br /><br />
 
-      <h2>Specific terms used in this policy:</h2>
-      "Time event": Time events are box-shaped elements that contain text and
-      images that you can define.<br />
-      "Timeline": This page allows you to create a series of time events. We
-      call this a timeline.
-      <br /><br />
+    <h2>Specific terms used in this policy:</h2>
+    "Time event": Time events are box-shaped elements that contain text and
+    images that you can define.<br />
+    "Timeline": This page allows you to create a series of time events. We call
+    this a timeline.
+    <br /><br />
 
-      <h2>What data do we collect?</h2>
-      When you agree to our privacy policy, we save the following data:<br />
-      - Your user id
-      <br /><br />
-      When you create a timeline we save the following data:<br />
-      - The id representing the timeline
-      <br /><br />
-      When you create a time event we save the following data:<br />
-      - The text you enter into the text fields<br />
-      - The images you submit<br />
-      - The id representing the time event and the ids of its images<br />
-      - The reference to the timeline
-      <br /><br />
+    <h2>What data do we collect?</h2>
+    When you agree to our privacy policy, we save the following data:<br />
+    - Your user id
+    <br /><br />
+    When you create a timeline we save the following data:<br />
+    - The id representing the timeline
+    <br /><br />
+    When you create a time event we save the following data:<br />
+    - The text you enter into the text fields<br />
+    - The images you submit<br />
+    - The id representing the time event and the ids of its images<br />
+    - The reference to the timeline
+    <br /><br />
 
-      <h2>How do we collect your data?</h2>
-      - We collect your user id after you agree to the privacy policy.<br />
-      - We collect your timeline id as soon as you create one.<br />
-      - We collect your time event data as soon as you create one.
-      <br /><br />
+    <h2>How do we collect your data?</h2>
+    - We collect your user id after you agree to the privacy policy.<br />
+    - We collect your timeline id as soon as you create one.<br />
+    - We collect your time event data as soon as you create one.
+    <br /><br />
 
-      <h2>How will we use your data?</h2>
-      We collect your data so that you can:<br />
-      - retrieve your timeline and time events after you close or refresh your
-      browser window.<br />
-      - share your timeline with others.
-      <br /><br />
+    <h2>How will we use your data?</h2>
+    We collect your data so that you can:<br />
+    - retrieve your timeline and time events after you close or refresh your
+    browser window.<br />
+    - share your timeline with others.
+    <br /><br />
 
-      <h2>How do we store your data?</h2>
-      We securely store your data at a cloud provider on servers in Germany.
-      <br /><br />
-      We will keep your data indefinitely or until you delete it via the delete
-      functions.<br />
-      We will also delete your data
-      <a href="#contact">upon your request</a>.<br />
-      We can decide to delete your data at any point for any reason, for example
-      if we run out of storage space.
-      <br /><br />
+    <h2>How do we store your data?</h2>
+    We securely store your data at a cloud provider on servers in Germany.
+    <br /><br />
+    We will keep your data indefinitely or until you delete it via the delete
+    functions.<br />
+    We will also delete your data
+    <a href="#contact">upon your request</a>.<br />
+    We can decide to delete your data at any point for any reason, for example
+    if we run out of storage space.
+    <br /><br />
 
-      <h2>Marketing</h2>
-      We do not share your data with any third party.<br />
-      We do not use your data for statistical or marketing purposes.
-      <br /><br />
+    <h2>Marketing</h2>
+    We do not share your data with any third party.<br />
+    We do not use your data for statistical or marketing purposes.
+    <br /><br />
 
-      <h2>What are your data protection rights?</h2>
-      You can alter and delete any data that you created with your user id on
-      our website.<br />
-      Further, you can make a request to us, to erase your data that we hold.
-      <br /><br />
-      Email us at:
-      <a href="mailto:contact.lookattime@gmail.com"
-        >contact.lookattime@gmail.com</a
-      >. <br /><br />
+    <h2>What are your data protection rights?</h2>
+    You can alter and delete any data that you created with your user id on our
+    website.<br />
+    Further, you can make a request to us, to erase your data that we hold.
+    <br /><br />
+    Email us at:
+    <a href="mailto:contact.lookattime@gmail.com"
+      >contact.lookattime@gmail.com</a
+    >. <br /><br />
 
-      <h2>Do we use Cookies?</h2>
-      We do not place cookies in your browser.
-      <br /><br />
+    <h2>Do we use Cookies?</h2>
+    We do not place cookies in your browser.
+    <br /><br />
 
-      <h2>Do we use local storage?</h2>
-      We use the local storage of your browser to store the following data on
-      your computer:<br />
-      - your user id<br />
-      - the id of your currently opened timeline<br />
-      - whether you agreed to the privacy policy
-      <br /><br />
+    <h2>Do we use local storage?</h2>
+    We use the local storage of your browser to store the following data on your
+    computer:<br />
+    - your user id<br />
+    - the id of your currently opened timeline<br />
+    - whether you agreed to the privacy policy
+    <br /><br />
 
-      <h2>How do we use local storage?</h2>
-      We use local storage for the following purposes:<br />
-      - to allow you retrieving previously created timelines and time events.<br />
-      - to show you the same timeline you last opened after you refresh or close
-      your browser window.<br />
-      - to check whether you have agreed to the privacy policy.
-      <br /><br />
-      We use local storage solely for the purpose of providing the functionality
-      of our service.<br />
-      We do not use local storage for any other purpose such as tracking or
-      advertising.
-      <br /><br />
+    <h2>How do we use local storage?</h2>
+    We use local storage for the following purposes:<br />
+    - to allow you retrieving previously created timelines and time events.<br />
+    - to show you the same timeline you last opened after you refresh or close
+    your browser window.<br />
+    - to check whether you have agreed to the privacy policy.
+    <br /><br />
+    We use local storage solely for the purpose of providing the functionality
+    of our service.<br />
+    We do not use local storage for any other purpose such as tracking or
+    advertising.
+    <br /><br />
 
-      <h2>How to manage local storage data?</h2>
-      You can delete the local storage by clearing your browser data.
-      <br /><br />
-      Please be aware that you will not be able to retrieve or alter your data
-      anymore if you remove your user id. The data you store on our service is
-      tied to your unique user id. It can not be accessed by another user id.<br />
-      In case you share a timeline, that timeline and its events can be
-      accessed, but not altered, by other users.
-      <br /><br />
+    <h2>How to manage local storage data?</h2>
+    You can delete the local storage by clearing your browser data.
+    <br /><br />
+    Please be aware that you will not be able to retrieve or alter your data
+    anymore if you remove your user id. The data you store on our service is
+    tied to your unique user id. It can not be accessed by another user id.<br />
+    In case you share a timeline, that timeline and its events can be accessed,
+    but not altered, by other users.
+    <br /><br />
 
-      <h2>Changes to our privacy policy</h2>
-      We keep our privacy policy under regular review and place any updates on
-      this web page. This privacy policy was last updated on 5 April 2021.
-      <br /><br />
+    <h2>Changes to our privacy policy</h2>
+    We keep our privacy policy under regular review and place any updates on
+    this web page. This privacy policy was last updated on 5 April 2021.
+    <br /><br />
 
-      <h2><a name="contact">How to contact us</a></h2>
-      If you have any questions about our privacy policy, the data we hold on
-      you, or you would like to exercise one of your data protection rights,
-      please do not hesitate to contact us.
-      <br /><br />
-      Write us at:<br />
-      Valerian Backeberg<br />
-      Allacher Str. 121<br />
-      80997 München<br />
-      <br />
-      Call us: +4917634515541<br />
-      <br />
-      Email us at:
-      <a href="mailto:contact.lookattime@gmail.com"
-        >contact.lookattime@gmail.com</a
-      >
-      <br /><br />
-    </div>
+    <h2><a name="contact">How to contact us?</a></h2>
+    If you have any questions about our privacy policy, the data we hold on you,
+    or you would like to exercise one of your data protection rights, please do
+    not hesitate to contact us.
+    <br /><br />
+    Write us at:<br />
+    Valerian Backeberg<br />
+    Allacher Str. 121<br />
+    80997 München<br />
+    <br />
+    Call us: +4917634515541<br />
+    <br />
+    Email us at:
+    <a href="mailto:contact.lookattime@gmail.com"
+      >contact.lookattime@gmail.com</a
+    >
+    <br /><br />
   </div>
 </template>
 

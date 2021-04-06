3faee75ae940561c6cecb575005ce40dd9ed78fb
Valerian Backeberg <valerian@backebergs.de>
2021-04-06 17:23:56 +0200

    In home show privacy policy dialog if not agreed to privacy policy

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="home">
     <prevent-mobile-dialog v-model="isMobile"></prevent-mobile-dialog>
-    <timeline ref="timelineElement"></timeline>
+    <timeline v-if="privacyPolicyAgreed" ref="timelineElement"></timeline>
+    <privacy-policy-dialog
+      v-model="showPrivacyPolicyDialog"
+      @setPrivacyPolicyAgreed="onSetPrivacyPolicyAgreed"
+    ></privacy-policy-dialog>
+    <div v-if="privacyPolicyDisagreed">
+      <p>
+        Sorry, you can only use this service after agreeing to the privacy
+        policy.
+      </p>
+      <p>
+        <router-link to="/privacy-policy">
+          Read the privacy policy.</router-link
+        >
+      </p>
+    </div>
   </div>
 </template>
 
@@ -9,17 +24,39 @@
 import Vue from "vue";
 import Timeline from "@/components/timeline/timeline.vue";
 import PreventMobileDialog from "@/components/prevent-mobile-dialog.vue";
+import PrivacyPolicyDialog from "@/components/privacy-policy-dialog.vue";
 
 export default Vue.extend({
   name: "Home",
 
   components: {
     Timeline,
-    PreventMobileDialog
+    PreventMobileDialog,
+    PrivacyPolicyDialog
+  },
+
+  created() {
+    this.privacyPolicyAgreed =
+      window.localStorage.getItem("privacyPolicyAgreed") === "true"
+        ? true
+        : false;
+    this.showPrivacyPolicyDialog = !this.privacyPolicyAgreed;
   },
 
   data() {
-    return { isMobile: navigator.maxTouchPoints > 1 };
+    return {
+      isMobile: navigator.maxTouchPoints > 1,
+      showPrivacyPolicyDialog: false,
+      privacyPolicyAgreed: false,
+      privacyPolicyDisagreed: false
+    };
+  },
+
+  methods: {
+    onSetPrivacyPolicyAgreed(value: boolean) {
+      this.privacyPolicyAgreed = value;
+      this.privacyPolicyDisagreed = !value;
+    }
   }
 });
 </script>

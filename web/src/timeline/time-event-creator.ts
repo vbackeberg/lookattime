21ca14bd469f33ddc1eac7c7346c0b1a8aee1da1
Valerian Backeberg <valerian@backebergs.de>
2021-04-09 15:25:19 +0200

    remove obsolete imports

--- web/src/timeline/time-event-creator.ts
+++ web/src/timeline/time-event-creator.ts
@@ -2,9 +2,6 @@ import TimeEventModel from "@/models/time-event-model";
 import store from "@/store/store";
 import Vue from "vue";
 import ViewFocuser from "./viewport/view-focuser";
-import PositionTranslator from "./position-translator";
-import { v4 as uuid } from "uuid";
-import ImageReferenceModel from "@/models/image-reference-model";
 
 /**
  * Adds timeEvent to store. Handles possible space extension. Scrolls to new timeEvent.

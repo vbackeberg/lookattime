e25d2973c2eee68e056cf0cc0e6c948199aa058d
Valerian Backeberg <valerian@backebergs.de>
2021-04-30 15:14:09 +0200

    add missing import

--- web/src/timeline/time-marker-management/time-marker-creator.ts
+++ web/src/timeline/time-marker-management/time-marker-creator.ts
@@ -2,6 +2,7 @@ import TimeMarkerModel from "@/models/time-marker-model";
 import store from "@/store/store";
 import { v4 as uuid } from "uuid";
 import PositionTranslator from "../position-translator";
+import VisibilityObserver from "../visibility-management/visibility-observer";
 import { Constants } from "./constants";
 
 /**

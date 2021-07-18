cdd2d4b2f008872bc43f24ef823396b361e94a57
Valerian Backeberg <valerian@backebergs.de>
2021-07-18 20:28:18 +0200

    Zoom observer: Unbuild debounce logic. (request animation frame)

--- web/src/timeline/zooming/zoom-observer.ts
+++ web/src/timeline/zooming/zoom-observer.ts
@@ -7,10 +7,9 @@ import Zoomer from "./zoomer";
 export default class ZoomObserver {
   private timelineElement: HTMLElement;
   private zoomer: Zoomer;
-  private numberOfWheelEvents = 0;
-  private ticking = false;
   private constructor() {
     this.zoomer = Zoomer.Instance;
+
     this.timelineElement = store.state.timelineElement;
     this.timelineElement.addEventListener("wheel", (e: WheelEvent) => {
       if (e.shiftKey || e.metaKey || e.ctrlKey || e.altKey) {
@@ -23,51 +22,15 @@ export default class ZoomObserver {
         return;
       }
 
-      this.numberOfWheelEvents += 1;
-      this.requestZoom(e);
+      this.changeZoom(e);
     });
   }
 
-  /**
-   * Calls change zoom with the current number of wheel events and resets the number of wheel events
-   * before the computation-heavy zoom operation is triggered.
-   *
-   * @param e wheel event containing the current cursor position and wheel direction
-   */
-  private requestZoom(e: WheelEvent) {
-    if (!this.ticking) {
-      this.ticking = true;
-      requestAnimationFrame(() => {
-        const numberOfWheelEvents = this.numberOfWheelEvents;
-        this.numberOfWheelEvents = 0;
-        this.changeZoom(e, numberOfWheelEvents);
-        // TODO: Here we should check whether there are wheel events left and call change zoom.
-        // Otherwise, if there is no new wheel event coming in, there will be unprocessed wheel events left.
-        // This will lead to
-        // 1. ...less zoom being applied than expected by the user
-        // 2. ...unexpectedly high zoom being applied the next time the user starts to zoom.
-        // In order to know what zoom to apply, we should store the last wheel event. 
-        this.ticking = false;
-      });
-    }
-  }
-
-  /**
-   * Calls the zoomer with the current cursor position.
-   *
-   * @param e wheel event containing the current cursor position and wheel direction
-   */
-  private changeZoom(e: WheelEvent, numberOfWheelEvents: number) {
+  private changeZoom(e: WheelEvent) {
     if (e.deltaY < 0) {
-      this.zoomer.zoom(
-        1.1 ** numberOfWheelEvents,
-        e.pageX + this.timelineElement.scrollLeft
-      );
+      this.zoomer.zoom(1.1, e.pageX + this.timelineElement.scrollLeft);
     } else if (e.deltaY > 0) {
-      this.zoomer.zoom(
-        0.92 ** numberOfWheelEvents,
-        e.pageX + this.timelineElement.scrollLeft
-      );
+      this.zoomer.zoom(0.92, e.pageX + this.timelineElement.scrollLeft);
     }
   }
 

fa7390a70e1d9e003021cfcb8d82354bf25477fe
Valerian Backeberg <valerian@backebergs.de>
2021-07-18 09:08:20 +0200

    Zoomer: Remove time marker html element null check as it is guaranteed to not be null

--- web/src/timeline/zooming/zoomer.ts
+++ web/src/timeline/zooming/zoomer.ts
@@ -115,10 +115,8 @@ export default class Zoomer {
       store.state.timeMarkers[i].positionCenter = newPosition;
 
       const element = store.state.timeMarkers[i].htmlElement;
-      if (element) {
-        element.style.transform =
-          "translateX(" + (newPosition - TimeMarkerModel.widthOffset) + "px)";
-      }
+      element.style.transform =
+        "translateX(" + (newPosition - TimeMarkerModel.widthOffset) + "px)";
     }
   }
 

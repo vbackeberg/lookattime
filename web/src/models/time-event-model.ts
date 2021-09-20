7fbbc3dbc3fd3773b3632dae0e1bc1e97354c7f9
Valerian Backeberg <valerian@backebergs.de>
2021-09-20 22:48:10 +0200

    time event html element: disable non null assertion

--- web/src/models/time-event-model.ts
+++ web/src/models/time-event-model.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import store from "@/store/store";
 import ImageReferenceModel from "./image-reference-model";
 import ExpansionState from "./time-event/expansion-state";

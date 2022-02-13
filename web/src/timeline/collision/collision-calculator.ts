189790e12356fd35605d4753f302a2646ed9f1cb
Valerian Backeberg <valerian@backebergs.de>
2022-02-13 20:53:49 +0100

    Collision Calculator: Change calculations according to new zoom level behavior

--- web/src/timeline/collision/collision-calculator.ts
+++ web/src/timeline/collision/collision-calculator.ts
@@ -1,5 +1,6 @@
 import TimeEventModel from "@/models/time-event-model";
 import store from "@/store/store";
+import { Constants } from "../zooming/constants";
 
 /**
  * Calculates zoom-level-dependent expansion states for every time event.
@@ -7,14 +8,19 @@ import store from "@/store/store";
 export default class CollisionCalculator {
   public static recalculateCollisions() {
     for (let i = 0; i < store.state.timeEvents.length; i++) {
-      const newExpansionZoomlevels = [0, 0, 0, 0];
+      const newExpansionZoomlevels = [
+        Constants.MAX_ZOOM_LEVEL,
+        Constants.MAX_ZOOM_LEVEL,
+        Constants.MAX_ZOOM_LEVEL,
+        Constants.MAX_ZOOM_LEVEL
+      ];
 
-      newExpansionZoomlevels[0] = Math.max(
+      newExpansionZoomlevels[0] = Math.min(
         this.findCollisionLeft(i, TimeEventModel.boxWidth),
         this.findCollisionRight(i, TimeEventModel.boxWidth)
       );
 
-      newExpansionZoomlevels[1] = Math.max(
+      newExpansionZoomlevels[1] = Math.min(
         this.findCollisionLeft(i, TimeEventModel.bubbleWidth),
         this.findCollisionRight(i, TimeEventModel.bubbleWidth)
       );
@@ -25,6 +31,13 @@ export default class CollisionCalculator {
     document.dispatchEvent(new Event("update-expansion-states"));
   }
 
+  /**
+   * Finds the nearest time event to the right, which has higher importance
+   * and should thus make this time event shrink.
+   *
+   * If no collision is found, it returns the maximum zoom level,
+   * which represents the maximum zoomed out view.
+   */
   private static findCollisionRight(
     currentTimeEventIndex: number,
     width: number
@@ -46,7 +59,7 @@ export default class CollisionCalculator {
       }
     }
 
-    return 0;
+    return Constants.MAX_ZOOM_LEVEL;
   }
 
   private static findCollisionLeft(
@@ -66,19 +79,24 @@ export default class CollisionCalculator {
       }
     }
 
-    return 0;
+    return Constants.MAX_ZOOM_LEVEL;
   }
 
+  /**
+   * Calculates the zoom level at which the given time events
+   * would collide.
+   */
   private static calculateCollisionZoomLevel(
     collidingTimeEvent: TimeEventModel,
     currentTimeEvent: TimeEventModel,
     width: number
   ) {
     return (
-      (store.state.zoomLevel * width) /
-      Math.abs(
+      store.state.zoomLevel *
+      (Math.abs(
         collidingTimeEvent.positionCenter - currentTimeEvent.positionCenter
-      )
+      ) /
+        width)
     );
   }
 }

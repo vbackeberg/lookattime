21d07334057c0009884bcb1848d8f50e6b8f141b
Valerian Backeberg <valerian@backebergs.de>
2022-03-13 11:15:47 +0100

    or connector remove obsolet css height style

--- web/src/components/timeline/time-event/time-event.vue
+++ web/src/components/timeline/time-event/time-event.vue
@@ -315,7 +315,6 @@ $scale-factor-dot-width: 0.05;
 }
 
 .connector {
-  height: 100%;
   width: 2px;
   background-color: #000;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);

df0a90a852014cb9903fb17cb109b1f85b3612a8
Valerian Backeberg <valerian@backebergs.de>
2022-06-11 12:54:56 +0200

    clarify max and min zoom level constants

--- web/src/timeline/zooming/constants.ts
+++ web/src/timeline/zooming/constants.ts
@@ -3,12 +3,15 @@ export class Constants {
    * The maximum zoom level corresponds to how many pixels
    * the browser allows on the horizontal axis, i.e. the
    * maximum value for translateX / position left.
+   *
+   * The maximum zoom level indicates that one absolute browser pixel
+   * will correspond to 1728000000000 relative units ()
    */
   public static MAX_ZOOM_LEVEL = 1728000000000;
 
   /**
-   * The minimum zoom level indicates that one relative unit
-   * (one second) will correspond to one absolute browser pixel.
+   * The minimum zoom level indicates that one absolute browser pixel
+   * will correspond to one relative unit (one second).
    */
   public static MIN_ZOOM_LEVEL = 1;
 }

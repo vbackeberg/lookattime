9904cc23c9f2ee9a2433c8ae87c05cde7bb02043
Valerian Backeberg <valerian@backebergs.de>
2022-06-26 13:39:46 +0200

    Add date rounding test ceil

--- web/tests/unit/temporal-rounding-extension.spec.ts
+++ web/tests/unit/temporal-rounding-extension.spec.ts
@@ -19,12 +19,13 @@ describe("TemporalRoundingExtension round", () => {
       second: 13,
       millisecond: 126,
       microsecond: 135,
-      nanosecond: 1034,
+      nanosecond: 104,
       timeZone: "UTC"
     })
   );
 
   const testCases: TestCase[] = [
+    // round to bottom (leftmost date) 
     {
       depth: "year",
       roundingMode: "trunc",
@@ -91,6 +92,78 @@ describe("TemporalRoundingExtension round", () => {
         hour: 15,
         minute: 14,
         second: 13,
+        millisecond: 0,
+        timeZone: "UTC"
+      })
+    },
+
+    // round to top (rightmost date) 
+    {
+      depth: "year",
+      roundingMode: "ceil",
+      expectedDate: Temporal.ZonedDateTime.from({
+        year: 2023,
+        month: 1,
+        day: 1,
+        timeZone: "UTC"
+      })
+    },
+    {
+      depth: "month",
+      roundingMode: "ceil",
+      expectedDate: Temporal.ZonedDateTime.from({
+        year: 2022,
+        month: 12,
+        day: 1,
+        timeZone: "UTC"
+      })
+    },
+    {
+      depth: "day",
+      roundingMode: "ceil",
+      expectedDate: Temporal.ZonedDateTime.from({
+        year: 2022,
+        month: 11,
+        day: 13,
+        hour: 0,
+        timeZone: "UTC"
+      })
+    },
+    {
+      depth: "hour",
+      roundingMode: "ceil",
+      expectedDate: Temporal.ZonedDateTime.from({
+        year: 2022,
+        month: 11,
+        day: 12,
+        hour: 16,
+        minute: 0,
+        timeZone: "UTC"
+      })
+    },
+    {
+      depth: "minute",
+      roundingMode: "ceil",
+      expectedDate: Temporal.ZonedDateTime.from({
+        year: 2022,
+        month: 11,
+        day: 12,
+        hour: 15,
+        minute: 15,
+        second: 0,
+        timeZone: "UTC"
+      })
+    },
+    {
+      depth: "second",
+      roundingMode: "ceil",
+      expectedDate: Temporal.ZonedDateTime.from({
+        year: 2022,
+        month: 11,
+        day: 12,
+        hour: 15,
+        minute: 14,
+        second: 14,
         millisecond: 0,
         timeZone: "UTC"
       })

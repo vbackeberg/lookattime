b811e9ef0607d9f9ded4c0beffaa51d4abaf7f8a
Valerian Backeberg <valerian@backebergs.de>
2022-07-03 11:01:52 +0200

    add js doc

--- web/src/timeline/viewport/viewport.ts
+++ web/src/timeline/viewport/viewport.ts
@@ -1,6 +1,10 @@
 import store from "@/store/store";
 
 export default class Viewport {
+  
+  /**
+   * @returns scrollLeft + clientWidth
+   */
   static rightEdge(): number {
     return (
       store.state.timelineElement.scrollLeft +

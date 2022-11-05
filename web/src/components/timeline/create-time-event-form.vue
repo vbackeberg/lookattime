b63dc43dd6a317352e6a2c346c20c834051aaa64
Valerian Backeberg <valerian@backebergs.de>
2022-11-05 18:44:01 +0100

    Create Form: Wysiwyg editor for text area with styles.

--- web/src/components/timeline/create-time-event-form.vue
+++ web/src/components/timeline/create-time-event-form.vue
@@ -117,16 +117,53 @@
                 </v-chip-group>
               </v-col>
               <v-col cols="12">
-                <v-textarea
-                  label="Text"
-                  required
-                  auto-grow
-                  outlined
-                  type="text"
-                  v-model="timeEvent.text"
-                  :rules="[ruleNotEmpty]"
-                /> </v-col
-            ></v-row> </v-container
+                <div v-if="editor">
+                  <v-btn-toggle class="editor-toolbar" dense multiple>
+                    <v-btn
+                      @click="
+                        editor
+                          .chain()
+                          .focus()
+                          .toggleBold()
+                          .run()
+                      "
+                      :disabled="
+                        !editor
+                          .can()
+                          .chain()
+                          .focus()
+                          .toggleBold()
+                          .run()
+                      "
+                      :class="{ 'is-active': editor.isActive('bold') }"
+                    >
+                      bold
+                    </v-btn>
+                    <v-btn
+                      @click="
+                        editor
+                          .chain()
+                          .focus()
+                          .toggleBold()
+                          .run()
+                      "
+                      :disabled="
+                        !editor
+                          .can()
+                          .chain()
+                          .focus()
+                          .toggleBold()
+                          .run()
+                      "
+                      :class="{ 'is-active': editor.isActive('bold') }"
+                    >
+                      bold
+                    </v-btn>
+                  </v-btn-toggle>
+                  <editor-content :editor="editor" />
+                </div>
+              </v-col>
+            </v-row> </v-container
         ></v-form>
       </v-card-text>
       <v-card-actions>
@@ -161,12 +198,36 @@ import { VForm } from "@/types";
 import ImageReferenceModel from "@/models/image-reference-model";
 import { getExtension } from "mime";
 import { Temporal } from "@js-temporal/polyfill";
+import { Editor, EditorContent } from "@tiptap/vue-2";
+import StarterKit from "@tiptap/starter-kit";
 
 export default Vue.extend({
   name: "CreateTimeEventForm",
 
+  components: {
+    EditorContent
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
+      extensions: [StarterKit],
+      editorProps: {
+        attributes: {
+          class: "editor-text-area"
+        }
+      }
+    });
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
+  },
+
   data() {
     return {
+      editor: (null as unknown) as Editor,
+
       images: [] as File[],
       imageReferencesToDelete: [] as ImageReferenceModel[],
       imageReferencesToAdd: [] as ImageReferenceModel[],
@@ -301,4 +362,33 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "src/colors.scss";
+
+.editor-toolbar {
+  margin-bottom: 8px;
+}
+
+/** This class represents the editor container. */
+.editor-text-area {
+  padding: 8px;
+
+  border-style: solid;
+  border-color: $lat-border-color;
+  border-radius: 4px;
+  border-width: 1px;
+
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.87);
+
+  &:hover {
+    border-color: $lat-border-color-hover;
+    border-width: 1px;
+  }
+
+  &:focus {
+    outline-color: $lat-primary-color;
+    outline-width: 2px;
+  }
+}
+</style>

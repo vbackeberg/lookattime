a664dae1cc61ac55c5a4ecf4b976ae54313b0767
Valerian Backeberg <valerian@backebergs.de>
2022-11-11 21:16:48 +0100

    Read mode: Pass in id

--- web/src/components/timeline/time-event/fullscreen/read-mode/text-area-read-mode.vue
+++ web/src/components/timeline/time-event/fullscreen/read-mode/text-area-read-mode.vue
@@ -1,13 +1,11 @@
-<!-- TODO not mutate prop -->
-<!-- eslint-disable vue/no-mutating-props -->
 <template>
   <div class="text-area">
     <div class="title-area">
-      <div class="event-title">{{ title }}</div>
+      <div class="event-title">{{ timeEvent.title }}</div>
       <div class="event-subtitle">{{ formattedDate }}</div>
       <!-- TODO: Maybe, add relationships to other dates here. (like wikipedia tags below title) -->
     </div>
-    <editor-read-mode v-model="text" />
+    <editor-read-mode v-bind:value="timeEvent.text" />
   </div>
 </template>
 
@@ -17,26 +15,36 @@ import EditorReadMode from "./editor-read-mode.vue";
 import { LOCALE } from "@/localization/locale";
 import DateTimeFormatOptions from "@/timeline/date-time-format-options";
 import { Temporal } from "@js-temporal/polyfill";
+import TimeEventModel from "@/models/time-event-model";
+import store from "@/store/store";
 
 export default Vue.extend({
   name: "TextAreaReadMode",
   components: { EditorReadMode },
   props: {
-    text: String,
-    title: String,
-    date: Number,
-    importance: Number,
-    imageReferences: Array
+    id: String,
+    value: Boolean
   },
 
   computed: {
-    formattedDate(): string {
-      return Temporal.Instant.fromEpochSeconds(this.date).toLocaleString(
-        LOCALE,
-        {
-          timeZone: Temporal.TimeZone.from(DateTimeFormatOptions.TIME_ZONE)
-        }
+    timeEvent(): TimeEventModel {
+      const index = store.state.timeEvents.findIndex(
+        timeEvent => timeEvent.id === this.id
       );
+
+      if (index !== -1) {
+        return store.state.timeEvents[index];
+      } else {
+        throw Error("Could not get time event index because it was not found");
+      }
+    },
+
+    formattedDate(): string {
+      return Temporal.Instant.fromEpochSeconds(
+        this.timeEvent.date
+      ).toLocaleString(LOCALE, {
+        timeZone: Temporal.TimeZone.from(DateTimeFormatOptions.TIME_ZONE)
+      });
     }
   }
 });

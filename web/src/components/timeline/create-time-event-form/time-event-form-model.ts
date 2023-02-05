27f4632196e779970a2c91c145ca84bdfc9d80c5
Valerian Backeberg <valerian@backebergs.de>
2023-02-05 15:55:21 +0100

    Remove obsolete form model

--- web/src/components/timeline/create-time-event-form/time-event-form-model.ts
+++
@@ -1,6 +0,0 @@
-import TimeEventModel from "@/models/time-event-model";
-
-export declare type TimeEventFormModel = Pick<
-  TimeEventModel,
-  "id" | "title" | "text" | "date" | "importance" | "imageReferences"
->;

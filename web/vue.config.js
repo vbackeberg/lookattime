301b9eba4a4e93374207037211eac201b15d55d9
Valerian Backeberg <valerian@backebergs.de>
2023-02-14 16:36:15 +0100

    disable source map

--- web/vue.config.js
+++ web/vue.config.js
@@ -3,13 +3,7 @@ module.exports = {
     allowedHosts: "all",
     proxy: "http://localhost:7071"
   },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
-      config.devtool = "false";
-    } else {
-      config.devtool = "source-map";
-    }
-  },
   lintOnSave: false,
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  productionSourceMap: false
 };

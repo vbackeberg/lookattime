a9ba90f6e7ecb0d8060656d6674ca83a4078ca97
Valerian Backeberg <valerian@backebergs.de>
2023-03-19 19:23:26 +0100

    add todo

--- web/src/timeline/viewport/view-focus-trigger.ts
+++ web/src/timeline/viewport/view-focus-trigger.ts
@@ -22,6 +22,18 @@ export default class ViewFocusTrigger {
         }
       }
 
+      /* TODO
+      Since zoom level in store is set to max at start, the second
+      event will always be inside view, thus view will stay max zoomed out.
+
+      Either set zoom level to min zoom level and fix the repositioning which
+      seems to be broken for an unknown reason.
+
+
+      Or: Change the view focuser logic such that it zooms in as much
+      as needed such that the time event is well visible (fully expanded).
+      */
+
       if (mutation.type === "addTimeEvent") {
         await Vue.nextTick();
 

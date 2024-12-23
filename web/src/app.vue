ca714fb4db9efa6dce056e3c594b94a84412d53a
Valerian <valerian@backebergs.de>
2024-12-23 15:24:15 +0100

    autoformat

--- web/src/app.vue
+++ web/src/app.vue
@@ -7,34 +7,21 @@
 
       <v-tooltip bottom transition="fade-transition">
         <template v-slot:activator="{ on }">
-          <v-btn text icon @click.stop="showWhatsNewDialog = true" v-on="on"
-            ><v-badge color="red" offset-y="8">
-              <v-icon x-large>mdi-alert-decagram</v-icon></v-badge
-            ></v-btn
-          >
+          <v-btn text icon @click.stop="showWhatsNewDialog = true" v-on="on"><v-badge color="red" offset-y="8">
+              <v-icon x-large>mdi-alert-decagram</v-icon></v-badge></v-btn>
         </template>
         <span>What's new?</span>
       </v-tooltip>
 
       <v-spacer />
 
-      <router-link to="/"
-        ><v-toolbar-title id="app-bar-title"
-          >Look at Time</v-toolbar-title
-        ></router-link
-      >
+      <router-link to="/"><v-toolbar-title id="app-bar-title">Look at Time</v-toolbar-title></router-link>
       <v-spacer />
       <v-menu offset-y>
         <template v-slot:activator="{ on: menu }">
           <v-tooltip bottom transition="fade-transition">
             <template v-slot:activator="{ on: tooltip }">
-              <v-avatar
-                v-ripple
-                color="primary"
-                id="app-bar-avatar"
-                v-on="{ ...menu, ...tooltip }"
-                >🤓</v-avatar
-              >
+              <v-avatar v-ripple color="primary" id="app-bar-avatar" v-on="{ ...menu, ...tooltip }">🤓</v-avatar>
             </template>
             <span>Your timelines</span>
           </v-tooltip>
@@ -42,16 +29,11 @@
         <v-list v-if="!loading">
           <v-list-item @click.stop="showManageTimelinesForm = true">
             <v-icon class="app-bar-menu-button">mdi-apps</v-icon>My
-            timelines</v-list-item
-          >
+            timelines</v-list-item>
           <v-list-item @click.stop="showShareDialog = true">
-            <v-icon class="app-bar-menu-button">mdi-share-variant</v-icon
-            >Share</v-list-item
-          >
+            <v-icon class="app-bar-menu-button">mdi-share-variant</v-icon>Share</v-list-item>
           <v-list-item @click.stop="startIntroduction()">
-            <v-icon class="app-bar-menu-button">mdi-help</v-icon
-            >Introduction</v-list-item
-          >
+            <v-icon class="app-bar-menu-button">mdi-help</v-icon>Introduction</v-list-item>
         </v-list>
       </v-menu>
     </v-app-bar>
@@ -62,23 +44,11 @@
 
     <v-footer app color="white" padless>
       <div class="footer-buttons">
-        <v-btn
-          text
-          rounded
-          x-small
-          class="my-2"
-          href="https://valerianb.medium.com"
-          target="_blank"
-        >
+        <v-btn text rounded x-small class="my-2" href="https://valerianb.medium.com" target="_blank">
           Blog
         </v-btn>
-        <v-btn
-          text
-          rounded
-          x-small
-          class="my-2"
-          href="mailto:contact.lookattime@gmail.com?subject=My%20feedback%20on%20Look%20At%20Time&amp;body=Feel%20free%20to%20include%20a%20screenshot%20of%20your%20issue."
-        >
+        <v-btn text rounded x-small class="my-2"
+          href="mailto:contact.lookattime@gmail.com?subject=My%20feedback%20on%20Look%20At%20Time&amp;body=Feel%20free%20to%20include%20a%20screenshot%20of%20your%20issue.">
           Feedback
         </v-btn>
         <v-btn text rounded x-small class="my-2" to="/legal-disclosure">

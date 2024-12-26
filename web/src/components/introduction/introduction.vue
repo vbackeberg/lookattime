44e7fe6ea33acb9054e2123cbe3320272bb6e2dd
Valerian <valerian@backebergs.de>
2024-12-26 17:21:35 +0100

    wip

--- web/src/components/introduction/introduction.vue
+++ web/src/components/introduction/introduction.vue
@@ -1,71 +1,34 @@
 <template>
   <div>
-    <step
-      :key="1"
-      v-if="step === 1"
-      v-on:next="next()"
-      anchorElementIdOrClass="fab"
-      location="left"
-      text="Look at Time allows you to create interactive timelines. Let's start by creating your first time event. Click here 👉"
-    ></step>
+    <step :key="1" v-if="step === 1" v-on:next="next()" anchorElementIdOrClass="fab" location="left"
+      text="Look at Time allows you to create interactive timelines. Let's start by creating your first time event. Click here 👉">
+    </step>
 
-    <step
-      :key="2"
-      v-if="step === 2"
-      v-on:next="next()"
-      anchorElementIdOrClass="event-title"
-      location="bottom"
-      text="This is your editor. You can add a title, a date and the importance of your event. Importance allows you to keep an overview when you zoom out. When two events come close to each other, the one with lower importance will fade out."
-    ></step>
+    <step :key="2" v-if="step === 2" v-on:next="next()" anchorElementIdOrClass="event-title" location="bottom"
+      text="This is your editor. You can add a title, a date and the importance of your event. Importance allows you to keep an overview when you zoom out. When two events come close to each other, the one with lower importance will fade out.">
+    </step>
 
-    <step
-      :key="3"
-      v-if="step === 3"
-      v-on:next="next()"
-      anchorElementIdOrClass="text-area-btn-save"
+    <step :key="3" v-if="step === 3" v-on:next="next()" anchorElementIdOrClass="text-area-btn-save" location="top"
+      text="Hit save when you have filled out every mandatory field."></step>
+
+    <step :key="4" v-if="step === 4" v-on:next="next()" anchorElementIdOrClass="content" triggerElementIdOrClass="fab"
       location="top"
-      text="Hit save when you have filled out every mandatory field."
-    ></step>
+      text="Great, you have created your first time event! Now, click the blue plus button on the lower right, once again. As soon as we have a second time event, you'll see the magic happen.">
+    </step>
 
-    <step
-      :key="4"
-      v-if="step === 4"
-      v-on:next="next()"
-      anchorElementIdOrClass="content"
-      triggerElementIdOrClass="fab"
-      location="top"
-      text="Great, you have created your first time event! Now, click the blue plus button on the lower right, once again. As soon as we have a second time event, you'll see the magic happen."
-    ></step>
+    <step :key="5" v-if="step === 5" v-on:next="next()" anchorElementIdOrClass="text-area-btn-save" location="top"
+      text="Fill out the fields and hit the save button. Make sure you pick a date that is a few days apart from the first one!">
+    </step>
 
-    <step
-      :key="5"
-      v-if="step === 5"
-      v-on:next="next()"
-      anchorElementIdOrClass="text-area-btn-save"
-      location="top"
-      text="Fill out the fields and hit the save button. Make sure you pick a date that is a few days apart from the first one!"
-    ></step>
+    <step :key="6" v-if="step === 6" v-on:next="next()" anchorElementIdOrClass="horizontal-line"
+      triggerElementIdOrClass="timeline" location="top" trigger="wheel"
+      text="Whoa! What happened? Your timeline has come to live. Below you can see where you are in time. Use your mouse wheel to zoom in and out.">
+    </step>
 
-    <step
-      :key="6"
-      v-if="step === 6"
-      v-on:next="next()"
-      anchorElementIdOrClass="horizontal-line"
-      triggerElementIdOrClass="timeline"
-      location="top"
-      trigger="wheel"
-      text="Whoa! What happened? Your timeline has come to live. Below you can see where you are in time. Use your mouse wheel to zoom in and out."
-    ></step>
-
-    <step
-      :key="7"
-      v-if="step === 7"
-      v-on:next="stopIntroduction()"
-      anchorElementIdOrClass="app-bar-avatar"
+    <step :key="7" v-if="step === 7" v-on:next="stopIntroduction()" anchorElementIdOrClass="app-bar-avatar"
       location="left"
       text="I have one last hint for you, traveler. Click on your avatar above to access your timelines and create new ones. Now, have a good flight!"
-      class="mt-14"
-    ></step>
+      class="mt-14"></step>
 
     <v-card id="dialog" rounded="xl" v-show="showDialog">
       <div id="title-container">
@@ -76,79 +39,45 @@
         </v-card-title>
       </div>
       <v-card-actions class="d-flex flex-column">
-        <v-btn block color="primary" @click="startIntroduction()"
-          >Take the tour</v-btn
-        >
-        <v-btn
-          block
-          color="tertiary"
-          @click="stopIntroduction()"
-          class="mt-3 ml-0"
-          >Close</v-btn
-        >
+        <v-btn block color="primary" @click="startIntroduction()">Take the tour</v-btn>
+        <v-btn block color="tertiary" @click="stopIntroduction()" class="mt-3 ml-0">Close</v-btn>
       </v-card-actions>
     </v-card>
 
-    <v-btn
-      v-show="step > 0"
-      id="btn-end"
-      color="secondary"
-      elevation="10"
-      @click="stopIntroduction()"
-      ><v-icon>mdi-close</v-icon>End Tour</v-btn
-    >
+    <v-btn v-show="step > 0" id="btn-end" color="secondary" elevation="10"
+      @click="stopIntroduction()"><v-icon>mdi-close</v-icon>End Tour</v-btn>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
+<script setup lang="ts">
 import Step from "@/components/introduction/step.vue";
-import store from "@/store/store";
-import { mapState } from "vuex";
+import { useLookAtTime } from "@/store/store";
+import { watch } from "vue";
 
-export default Vue.extend({
-  name: "Introduction",
+let showDialog = true
+let step = 0
 
-  components: {
-    Step
-  },
+const store = useLookAtTime()
 
-  data() {
-    return {
-      showDialog: true,
-      step: 0
-    };
-  },
-
-  computed: { ...mapState(["timeEvents"]) },
-
-  watch: {
-    timeEvents: {
-      immediate: true,
-      handler(timeEvents) {
-        if (timeEvents.length > 0 && this.step === 0) {
-          store.commit("setShowIntroduction", false);
-        }
-      }
-    }
-  },
-
-  methods: {
-    startIntroduction() {
-      this.showDialog = false;
-      this.step = 1;
-    },
-
-    stopIntroduction() {
-      this.step = 0;
-      store.commit("setShowIntroduction", false);
-    },
-
-    next() {
-      this.step++;
-    }
+watch(store.timeEvents, async (newTimeEvents) => {
+  if (newTimeEvents.length > 0 && step === 0) {
+    store.showIntroduction = false;
   }
 });
+
+function startIntroduction() {
+  showDialog = false;
+  step = 1;
+}
+
+function stopIntroduction() {
+  step = 0;
+  store.showIntroduction = false;
+}
+
+function next() {
+  step++;
+}
 </script>
 
 <style lang="scss" scoped>
--- web/src/components/introduction/step.vue
+++ web/src/components/introduction/step.vue
@@ -1,159 +1,151 @@
 <template>
   <transition :name="`slide-${location}`">
-    <v-card elevation="10" class="step-card" v-show="elementsFound"
-      ><v-card-text class="black--text">{{ text }}</v-card-text></v-card
-    >
+    <v-card elevation="10" class="step-card" v-show="elementsFound"><v-card-text class="black--text">{{ text
+        }}</v-card-text></v-card>
   </transition>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
+<script setup lang="ts">
+import { onMounted } from 'vue';
 
-export default Vue.extend({
-  name: "Step",
 
-  props: {
-    text: { type: String, required: true },
+const props = defineProps({
+  text: { type: String, required: true },
 
-    /** The element to position the step at */
-    anchorElementIdOrClass: { type: String, required: true },
-
-    /**
-     * The element to trigger the next step.
-     * If undefined, the `anchorElement` triggers the next step
-     */
-    triggerElementIdOrClass: String,
-
-    /** Defines where to place the step card around the anchor element. */
-    location: {
-      type: String,
-      required: true,
-      validator: (v: string) => ["top", "bottom", "left", "right"].includes(v)
-    },
-
-    /** Any event type such as "wheel" */
-    trigger: { type: String, default: "click" }
-  },
-
-  data() {
-    return {
-      elementsFound: false,
-      anchorElement: undefined as DOMRect | null | undefined,
-      triggerElement: undefined as Element | null | undefined,
-      mutationObserver: {} as MutationObserver
-    };
-  },
+  /** The element to position the step at */
+  anchorElementIdOrClass: { type: String, required: true },
 
   /**
-   * Starts trying to position the step component at the right location.
-   * The `anchorElement` may appear in the DOM before or after
-   * the step component has been created.
+   * The element to trigger the next step.
+   * If undefined, the `anchorElement` triggers the next step
    */
-  mounted() {
-    const observer = new MutationObserver(() => {
-      this.tryPlaceNearAnchorElement(observer);
-    });
+  triggerElementIdOrClass: String,
 
-    this.tryPlaceNearAnchorElement(observer);
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true
-    });
+  /** Defines where to place the step card around the anchor element. */
+  location: {
+    type: String,
+    required: true,
+    validator: (v: string) => ["top", "bottom", "left", "right"].includes(v)
   },
 
-  methods: {
+  /** Any event type such as "wheel" */
+  trigger: { type: String, default: "click" }
+})
+
+let elementsFound = false
+let anchorElement = undefined as DOMRect | null | undefined
+let triggerElement = undefined as Element | null | undefined
+let mutationObserver = {} as MutationObserver
+
+/**
+ * Starts trying to position the step component at the right location.
+ * The `anchorElement` may appear in the DOM before or after
+ * the step component has been created.
+ */
+onMounted(() => {
+  const observer = new MutationObserver(() => {
+    tryPlaceNearAnchorElement(observer);
+  });
+
+  tryPlaceNearAnchorElement(observer);
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+})
+
     /**
      * Places the element next to the `anchorElement` if it is present.
      */
-    async tryPlaceNearAnchorElement(mutationObserver: MutationObserver) {
-      this.anchorElement = this.getElementByIdOrClass(
-        this.anchorElementIdOrClass
-      )?.getBoundingClientRect();
+    async function tryPlaceNearAnchorElement(mutationObserver: MutationObserver) {
+    anchorElement = getElementByIdOrClass(
+      anchorElementIdOrClass
+    )?.getBoundingClientRect();
 
-      this.triggerElement = this.getElementByIdOrClass(
-        this.triggerElementIdOrClass ?? this.anchorElementIdOrClass
-      );
+    triggerElement = getElementByIdOrClass(
+      triggerElementIdOrClass ?? anchorElementIdOrClass
+    );
 
-      // Do not proceed until required elements are found.
-      if (!this.anchorElement || !this.triggerElement) return;
+    // Do not proceed until required elements are found.
+    if (!anchorElement || !triggerElement) return;
 
-      this.elementsFound = true;
+    elementsFound = true;
 
-      mutationObserver.disconnect();
+    mutationObserver.disconnect();
 
-      // Element isn't yet rendered, which would make it's height 0.
-      await this.$nextTick();
+    // Element isn't yet rendered, which would make it's height 0.
+    await $nextTick();
 
-      const margin = 24;
+    const margin = 24;
 
-      switch (this.location) {
-        case "top": {
-          (this.$el as HTMLElement).style.bottom =
-            document.documentElement.clientHeight -
-            this.anchorElement.top +
-            margin +
-            "px";
+    switch (location) {
+      case "top": {
+        ($el as HTMLElement).style.bottom =
+          document.documentElement.clientHeight -
+          anchorElement.top +
+          margin +
+          "px";
 
-          this.horizontalAlignCenter();
-          break;
-        }
-        case "bottom": {
-          (this.$el as HTMLElement).style.top =
-            this.anchorElement.bottom + margin + "px";
-
-          this.horizontalAlignCenter();
-          break;
-        }
-        case "left": {
-          (this.$el as HTMLElement).style.right =
-            document.documentElement.clientWidth -
-            this.anchorElement.left +
-            margin +
-            "px";
-
-          this.verticalAlignCenter();
-          break;
-        }
-        case "right": {
-          (this.$el as HTMLElement).style.left =
-            this.anchorElement.right + margin + "px";
-
-          this.verticalAlignCenter();
-          break;
-        }
+        horizontalAlignCenter();
+        break;
       }
+      case "bottom": {
+        ($el as HTMLElement).style.top =
+          anchorElement.bottom + margin + "px";
 
-      this.triggerElement.addEventListener(this.trigger, () => {
-        this.$emit("next");
-      });
-    },
+        horizontalAlignCenter();
+        break;
+      }
+      case "left": {
+        ($el as HTMLElement).style.right =
+          document.documentElement.clientWidth -
+          anchorElement.left +
+          margin +
+          "px";
 
-    verticalAlignCenter() {
-      (this.$el as HTMLElement).style.top =
-        this.anchorElement!!.top +
-        this.anchorElement!!.height / 2 -
-        (this.$el as HTMLElement).offsetHeight / 2 +
-        "px";
+        verticalAlignCenter();
+        break;
+      }
+      case "right": {
+        ($el as HTMLElement).style.left =
+          anchorElement.right + margin + "px";
 
-      console.log((this.$el as HTMLElement).style.top);
-    },
-
-    horizontalAlignCenter() {
-      (this.$el as HTMLElement).style.left =
-        this.anchorElement!!.left +
-        this.anchorElement!!.width / 2 -
-        (this.$el as HTMLElement).offsetWidth / 2 +
-        "px";
-    },
-
-    getElementByIdOrClass(elementIdOrClass: string): Element | null {
-      return (
-        document.getElementById(elementIdOrClass) ??
-        document.getElementsByClassName(elementIdOrClass)[0]
-      );
+        verticalAlignCenter();
+        break;
+      }
     }
+
+    triggerElement.addEventListener(trigger, () => {
+      $emit("next");
+    });
+  },
+
+  verticalAlignCenter() {
+    ($el as HTMLElement).style.top =
+      anchorElement!!.top +
+      anchorElement!!.height / 2 -
+      ($el as HTMLElement).offsetHeight / 2 +
+      "px";
+
+    console.log(($el as HTMLElement).style.top);
+  },
+
+  horizontalAlignCenter() {
+    ($el as HTMLElement).style.left =
+      anchorElement!!.left +
+      anchorElement!!.width / 2 -
+      ($el as HTMLElement).offsetWidth / 2 +
+      "px";
+  },
+
+  getElementByIdOrClass(elementIdOrClass: string): Element | null {
+    return (
+      document.getElementById(elementIdOrClass) ??
+      document.getElementsByClassName(elementIdOrClass)[0]
+    );
   }
+}
 });
 </script>
 <style lang="scss" scoped>
@@ -173,6 +165,7 @@ $translatePx: 10px;
 .slide-left-leave-active {
   transition: all $transitionDuration;
 }
+
 .slide-left-enter,
 .slide-left-leave-to {
   transform: translateX(-$translatePx);
@@ -183,6 +176,7 @@ $translatePx: 10px;
 .slide-right-leave-active {
   transition: all $transitionDuration;
 }
+
 .slide-right-enter,
 .slide-right-leave-to {
   transform: translateX($translatePx);
@@ -193,6 +187,7 @@ $translatePx: 10px;
 .slide-top-leave-active {
   transition: all $transitionDuration;
 }
+
 .slide-top-enter,
 .slide-top-leave-to {
   transform: translateY(-$translatePx);
@@ -203,6 +198,7 @@ $translatePx: 10px;
 .slide-bottom-leave-active {
   transition: all $transitionDuration;
 }
+
 .slide-bottom-enter,
 .slide-bottom-leave-to {
   transform: translateY($translatePx);

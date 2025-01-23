3b6576957508ab82a01317eee1615bb06e183f77
Valerian Backeberg <valerian@backebergs.de>
2025-01-23 20:05:11 +0100

    Remove obsolete loading of vuetify components because they are loaded through vuetify.

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,20 +1,11 @@
 import { createApp } from 'vue'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as labsComponents from 'vuetify/labs/components'
-import * as directives from 'vuetify/directives'
 import App from './app.vue'
 import router from './router'
 import { createPinia } from 'pinia';
 
-const vuetify = createVuetify({
-  components: {
-    ...components, ...labsComponents
-  },
-  directives,
-})
-
+const vuetify = createVuetify()
 const pinia = createPinia()
 
 createApp(App)

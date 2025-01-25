a36ebf5331118b9a646417cbccdfaf90bce01d9b
Valerian Backeberg <valerian@backebergs.de>
2025-01-25 15:13:01 +0100

    install tailwind

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: { transformAssetUrls },
-    }), Vuetify() ],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    Vuetify(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

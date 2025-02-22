f1f3122c15835145d29d6d25d916ec1959ede31a
Valerian Backeberg <valerian@backebergs.de>
2025-02-22 12:49:36 +0100

    WIP: pass ref pos and zoom factor to time event and on zoom, when it changes, change time events position.

--- src/lib/components/timeline/time-event.svelte
+++ src/lib/components/timeline/time-event.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
+	import { getContext, untrack } from 'svelte';
 
-	let { timeEvent }: { timeEvent: TimeEvent } = $props();
 	const zoomLevel = getContext<{ v: number }>('zoomLevel');
 
-	
+	let {
+		timeEvent,
+		referencePosition,
+		zoomFactor
+	}: { timeEvent: TimeEvent; referencePosition: number; zoomFactor: number } = $props();
 
+	let position = $state(timeEvent.date / zoomLevel.v);
+
+	$effect(() => {
+		const distance = (untrack(() => position) - referencePosition) * zoomFactor;
+		position = referencePosition + distance;
+	});
+
+	// when position negative push to positive and shift viewport within 1 frame
+
+	//
 </script>
 
 <div class="w-40 rounded border border-dashed" style={`transform: translateX(${position}px)`}>
 	<div class="w-full -translate-x-1/2 rounded border">
 		time event
 		{position}
+		{timeEvent.date}
 	</div>
 </div>
--- src/lib/components/timeline/timeline.svelte
+++ src/lib/components/timeline/timeline.svelte
@@ -2,28 +2,31 @@
 	import TimeEvent from './time-event.svelte';
 	import { page } from '$app/state';
 	import { setContext } from 'svelte';
+	import { toPosition } from '$lib/position-translator.svelte';
 
 	const MAX_ZOOM_LEVEL = 1_728_000_000_000;
 	const MIN_ZOOM_LEVEL = 1;
-	const zoomLevel = $state({ v: 400 });
-	setContext('zoomLevel', zoomLevel);
+	const zoomLevel = $state({ v: 1_000 });
+	const offset = $state({ v: 0 });
+	let referencePosition = $state(0);
+	let zoomFactor = $state(1);
 
-	
+	setContext('zoomLevel', zoomLevel);
+	setContext('offset', offset);
+
+	const distanceFirstTe = toPosition(page.data.timeEvents[0].date, zoomLevel.v, offset.v);
+	// 1732002 px
+	offset.v -= distanceFirstTe;
 
 	function zoom(e: WheelEvent & { currentTarget: EventTarget & HTMLDivElement }) {
 		if (e.shiftKey || e.metaKey || e.ctrlKey || e.altKey) return;
 		if (page.data.timeEvents.length === 0) return;
 		e.preventDefault();
-		const zoomFactor = e.deltaY < 0 ? 0.92 : 1.1
+		zoomFactor = e.deltaY < 0 ? 0.92 : 1.1;
 		const newZoomLevel = zoomLevel.v * zoomFactor;
 		if (zoomLevelInBounds(newZoomLevel)) zoomLevel.v = newZoomLevel;
 
-		const referencePosition = e.pageX
-
-		timeEvents.forEach(t => {
-			const distance = (t.position - referencePosition) * zoomFactor
-			t.position = referencePosition + distance
-		});
+		referencePosition = e.pageX;
 	}
 
 	function zoomLevelInBounds(newZoomLevel: number) {
@@ -32,9 +35,13 @@
 </script>
 
 <div class="size-full" onwheel={zoom}>
-	<div>
-		{#each timeEvents as timeEvent}
-			<TimeEvent {timeEvent}></TimeEvent>
-		{/each}
+	{#each page.data.timeEvents as timeEvent}
+		<TimeEvent {timeEvent} {referencePosition} {zoomFactor}></TimeEvent>
+	{/each}
+
+	<div class="mt-16 flex flex-col gap-2">
+		<h2 class="text-xl">Debug</h2>
+		<span>Zoom Level {zoomLevel.v}</span>
+		<span>Offset {offset.v}</span>
 	</div>
 </div>

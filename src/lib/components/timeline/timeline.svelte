bdcb0de9787b82aa566b6814d47cfd76b00b64a2
Valerian Backeberg <valerian@backebergs.de>
2025-02-23 14:53:10 +0100

    move all right and scroll same amount, when lowest te has negative position. listen to window onwheel, because timeline div doesn't span full width

--- src/lib/components/timeline/timeline.svelte
+++ src/lib/components/timeline/timeline.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import TimeEvent from './time-event.svelte';
 	import { page } from '$app/state';
-	import { untrack } from 'svelte';
+	import { onMount, tick, untrack } from 'svelte';
 
 	const MAX_ZOOM_LEVEL = 1_728_000_000_000;
 	const MIN_ZOOM_LEVEL = 1;
 	let referencePosition = $state(0);
 	let zoomFactor = $state(1);
 	let zoomLevel = $state(1_000);
-	let positionLowest = $state(0)
+	let positionLowest = $state(0);
 
 	$effect(() => {
 		zoomLevel;
@@ -16,7 +16,7 @@
 		positionLowest = referencePosition + distance;
 	});
 
-	function zoom(e: WheelEvent & { currentTarget: EventTarget & HTMLDivElement }) {
+	function zoom(e: WheelEvent) {
 		if (e.shiftKey || e.metaKey || e.ctrlKey || e.altKey) return;
 		if (page.data.timeEvents.length === 0) return;
 		e.preventDefault();
@@ -30,15 +30,23 @@
 		return Math.abs(newZoomLevel) < MAX_ZOOM_LEVEL && Math.abs(newZoomLevel) >= MIN_ZOOM_LEVEL;
 	}
 
-	let offset = $state(0);
-
 	$effect(() => {
-		// offset = -positionDateZero;
-		// window.scrollBy(-positionDateZero, 0);
+		if (positionLowest < 0) {
+			const distance = -positionLowest;
+			tick().then(() => {
+				positionLowest += distance;
+				tick().then(() => {
+					console.log('scroll');
+					window.scrollBy(distance, 0);
+				});
+			});
+		}
 	});
 </script>
 
-<div class="size-full" onwheel={zoom}>
+<svelte:window onwheel={zoom} />
+
+<div class="size-full">
 	{#each page.data.timeEvents as timeEvent, i}
 		<TimeEvent {timeEvent} {zoomLevel} {positionLowest}></TimeEvent>
 	{/each}
@@ -47,6 +55,5 @@
 		<h2 class="text-xl">Debug</h2>
 		<span>Zoom Level {zoomLevel}</span>
 		<span>positionLowest {positionLowest}</span>
-		<span>Offset {offset}</span>
 	</div>
 </div>

ee40893e4b12eecb029f2917fd3f18cbb7b5a0db
Valerian Backeberg <valerian@backebergs.de>
2025-02-23 18:33:36 +0100

    format

--- src/lib/components/timeline/time-event.svelte
+++ src/lib/components/timeline/time-event.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-	import { page } from "$app/state";
+	import { page } from '$app/state';
 
 	let {
 		timeEvent,
 		zoomLevel,
-		positionLowest,
+		positionLowest
 	}: {
 		timeEvent: TimeEvent;
 		zoomLevel: number;
 		positionLowest: number;
 	} = $props();
 
-	let position = $derived(positionLowest + (timeEvent.date - page.data.timeEvents[0].date) / zoomLevel);
+	let position = $derived(
+		positionLowest + (timeEvent.date - page.data.timeEvents[0].date) / zoomLevel
+	);
 </script>
 
-<div
-	class="w-40 h-20 rounded border border-dashed"
-	style={`transform: translateX(${position}px)`}
->
+<div class="h-20 w-40 rounded border border-dashed" style={`transform: translateX(${position}px)`}>
 	<div class="size-full -translate-x-1/2 rounded border">
 		time event
 		{position}

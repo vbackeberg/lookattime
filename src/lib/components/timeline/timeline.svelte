97c1755231db6d649592dd1e9f3252c964e7cec6
Valerian Backeberg <valerian@backebergs.de>
2025-02-23 17:49:16 +0100

    determine zoomlevel on start

--- src/lib/components/timeline/timeline.svelte
+++ src/lib/components/timeline/timeline.svelte
@@ -7,13 +7,28 @@
 	const MIN_ZOOM_LEVEL = 1;
 	let referencePosition = $state(0);
 	let zoomFactor = $state(1);
-	let zoomLevel = $state(1_000);
+	let zoomLevel = $state(0);
 	let positionLowest = $state(0);
 
+	$effect.pre(() => {
+		const lowestDate = page.data.timeEvents[0].date;
+		const highestDate = page.data.timeEvents[page.data.timeEvents.length - 1].date;
+		zoomLevel = (highestDate - lowestDate) / window.innerWidth;
+	});
+
 	$effect(() => {
-		zoomLevel;
-		const distance = (untrack(() => positionLowest) - referencePosition) / zoomFactor;
-		positionLowest = referencePosition + distance;
+		if (zoomLevel) {
+			const distance = (untrack(() => positionLowest) - referencePosition) / zoomFactor;
+			positionLowest = referencePosition + distance;
+		}
+	});
+
+	$effect(() => {
+		if (positionLowest < 0) {
+			const distance = -positionLowest;
+			positionLowest += distance;
+			window.scrollBy(distance, 0);
+		}
 	});
 
 	function zoom(e: WheelEvent) {
@@ -29,23 +44,17 @@
 	function zoomLevelInBounds(newZoomLevel: number) {
 		return Math.abs(newZoomLevel) < MAX_ZOOM_LEVEL && Math.abs(newZoomLevel) >= MIN_ZOOM_LEVEL;
 	}
-
-	$effect(() => {
-		if (positionLowest < 0) {
-			const distance = -positionLowest;
-			positionLowest += distance;
-			window.scrollBy(distance, 0);
-		}
-	});
 </script>
 
 <svelte:window onwheel={zoom} />
 
-<div class="size-full">
-	{#each page.data.timeEvents as timeEvent, i}
-		<TimeEvent {timeEvent} {zoomLevel} {positionLowest}></TimeEvent>
-	{/each}
-</div>
+{#if zoomLevel}
+	<div class="size-full">
+		{#each page.data.timeEvents as timeEvent, i}
+			<TimeEvent {timeEvent} {zoomLevel} {positionLowest}></TimeEvent>
+		{/each}
+	</div>
+{/if}
 
 <div class="fixed bottom-0 flex flex-col gap-2 bg-orange-200/50 p-4">
 	<h2 class="text-xl">Debug</h2>

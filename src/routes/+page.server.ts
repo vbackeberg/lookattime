52594acf4d5c8dc4aeb0713c0e2c53507ba91708
Valerian Backeberg <valerian@backebergs.de>
2025-02-23 14:13:48 +0100

    server: send more time events

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,6 +7,38 @@ export async function load() {
         id: "9081f515-a072-45dd-b6ab-15f971578092",
         date: 1_737_581_730,
         text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_735_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_736_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_737_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_734_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_733_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_732_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_731_581_730,
+        text: "text 2"
+    }, {
+        id: "9081f515-a072-45dd-b6ab-15f971578092",
+        date: 1_730_581_730,
+        text: "text 2"
     }].toSorted((a, b) => a.date - b.date)
 
     return { timeEvents }

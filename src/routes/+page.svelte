43b373de8a77dfa8c191f3a9f1a08fb44a44fac0
Valerian Backeberg <valerian@backebergs.de>
2025-03-30 18:11:08 +0200

    props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<script>
+<script lang="ts">
 	import Timeline from '$lib/components/timeline/timeline.svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
 </script>
 
-<Timeline />
+<Timeline timeEvents={data.timeEvents} />

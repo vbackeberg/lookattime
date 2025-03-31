eb5ffe36465f9a3c6a789ac77558b6890f4305bf
Valerian Backeberg <valerian@backebergs.de>
2025-03-31 16:13:48 +0200

    Implement basic canvas scrollbar

--- src/lib/components/timeline/timeline.svelte
+++ src/lib/components/timeline/timeline.svelte
@@ -4,21 +4,13 @@
 	import { onMount } from 'svelte';
 	let { timeEvents }: { timeEvents: TimeEvent[] } = $props();
 
-	const zoomLevel = 10_000_000;
+	const zoomLevel = 1;
 
 	let stage: Konva.Stage;
 	let layer: Konva.Layer;
 	let elements: Rect[];
 
 	onMount(() => {
-		stage = new Konva.Stage({
-			container: 'container',
-			width: 500,
-			height: 500
-		});
-
-		layer = new Konva.Layer();
-
 		elements = timeEvents.map(
 			(t) =>
 				new Konva.Rect({
@@ -31,10 +23,54 @@
 				})
 		);
 
+		scrollWidth = determineWidth();
+
+		stage = new Konva.Stage({
+			container: 'container',
+			width: innerWidth,
+			height: innerHeight
+		});
+
+		layer = new Konva.Layer();
+
 		layer.add(...elements);
 		stage.add(layer);
 		stage.on('wheel', (e) => {
 			zoom(e.evt);
+		});
+
+		const scrollbarLayer = new Konva.Layer();
+		stage.add(scrollbarLayer);
+
+		const PADDING = 4;
+
+		const horizontalBar = new Konva.Rect({
+			width: 100,
+			height: 10,
+			fill: 'grey',
+			opacity: 0.8,
+			x: PADDING,
+			y: stage.height() - PADDING - 10,
+			draggable: true,
+			dragBoundFunc: function (pos) {
+				pos.x = Math.max(Math.min(pos.x, stage.width() - this.width() - PADDING), PADDING);
+				pos.y = stage.height() - PADDING - 10;
+
+				return pos;
+			}
+		});
+		scrollbarLayer.add(horizontalBar);
+
+		horizontalBar.on('dragmove', function () {
+			console.log('pos layer x: ' + layer.x());
+			const availableWidth = stage.width() - PADDING * 2 - horizontalBar.width();
+			console.log('availableWidth ' + availableWidth);
+			const delta = (horizontalBar.x() - PADDING) / availableWidth;
+			console.log('delta ' + delta);
+
+			layer.x(-(scrollWidth! - stage.width()) * delta);
+			console.log('new pos: ' + -(scrollWidth! - stage.width()) * delta);
+			console.log('pos layer x ' + layer.x());
 		});
 	});
 
@@ -47,17 +83,29 @@
 		if (!pointer) return;
 
 		const zoomFactor = e.deltaY > 0 ? 1 / 1.1 : 1.1;
-
 		elements.forEach((e) => {
 			const distance = (e.x() - pointer.x) * zoomFactor;
 			e.x(pointer.x + distance);
 		});
+
+		scrollWidth = determineWidth();
 	}
 
-	let innerWidth = $state<number>()
-	$effect(() => {stage.width(innerWidth)})
+	function determineWidth() {
+		const positionLowest = Math.min(elements[0].position().x, 0);
+		const positionHighest = Math.max(elements[elements.length - 1].position().x, innerWidth!);
+		return positionHighest - positionLowest;
+	}
+
+	let scrollWidth = 0;
+	let innerWidth = $state<number>();
+	let innerHeight = $state<number>();
+	$effect(() => {
+		stage.width(innerWidth);
+		stage.height(innerHeight);
+	});
 </script>
 
-<svelte:window bind:innerWidth />
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <div id="container"></div>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
 export async function load() {
     const timeEvents: TimeEvent[] = [{
         id: "1b686c64-fc61-4ee2-ab18-0e320b108efe",
-        date: 1_738_964_130,
+        date: 500,
         text: "text 1"
     }, {
         id: "9081f515-a072-45dd-b6ab-15f971578092",
-        date: 1_737_581_730,
+        date: 1500,
         text: "text 2"
     }].toSorted((a, b) => a.date - b.date)
 
